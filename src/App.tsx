1ec5e465be88304b5d2d4b6052a32ff7f4d2727c
Ajin <ajin@Ajinui-noteubug.local>
2025-06-25 08:18:55 +0900

    style: Pretendard 폰트 기본 적용

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import GlobalStyle from './GlobalStyle';
 function App() {
   return <>
     <GlobalStyle />
+    <div>스타일 확인용!</div>
   </>;
 }
 
--- src/GlobalStyle.tsx
+++ src/GlobalStyle.tsx
@@ -3,7 +3,9 @@ import { css, Global } from '@emotion/react';
 const GlobalStyle = () => (
   <Global
     styles={css`
-      /* reset */
+      /* Pretendard 웹폰트 import */
+      @import url('https://cdn.jsdelivr.net/gh/orioncactus/pretendard/dist/web/static/pretendard.css');
+
       * {
         margin: 0;
         padding: 0;
@@ -12,6 +14,9 @@ const GlobalStyle = () => (
 
       html,
       body {
+        font-family: 'Pretendard', -apple-system, BlinkMacSystemFont, system-ui,
+          Roboto, 'Helvetica Neue', 'Segoe UI', 'Apple SD Gothic Neo', 'Noto Sans KR',
+          'Malgun Gothic', sans-serif;
         background-color: #ffffff;
         color: #000000;
       }
