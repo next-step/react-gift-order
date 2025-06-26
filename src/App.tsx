7e95e8efdb92db46ad6b72d735e7a6516f929736
Ajin <ajin@Ajinui-noteubug.local>
2025-06-26 22:06:34 +0900

    feat: 선물할 친구 선택 섹션 구현

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import GlobalStyle from '@/styles/GlobalStyle';
 import NavigationBar from '@/components/NavigationBar/NavigationBar';
 import CategorySection from '@/components/Category/CategorySection';
 import SectionTitle from '@/components/SectionTitle';
+import GiftReceiverSection from './components/GiftReceiverSelect/GiftReceiverSelect';
 
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
     <>
       <GlobalStyle />
       <NavigationBar />
+      <GiftReceiverSection/>
       <SectionTitle title="선물 테마" />
       <CategorySection />
       <SectionTitle title="실시간 급상승 선물랭킹" />
