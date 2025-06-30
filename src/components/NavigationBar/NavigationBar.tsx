d6dd4e7183950fbf7bd68e6775623c644a888513
Ajin <ajin@Ajinui-noteubug.local>
2025-07-01 05:01:55 +0900

    feat: 네비게이션 아이콘에 기능 추가 (뒤로가기, 로그인 페이지 이동)

--- src/components/NavigationBar/NavigationBar.tsx
+++ src/components/NavigationBar/NavigationBar.tsx
@@ -1,12 +1,23 @@
 import { ChevronLeft, UserRound } from 'lucide-react';
 import * as S from './NavigationBar.styles';
+import { useNavigate } from 'react-router-dom';
 
 const NavigationBar = () => {
+  const navigate = useNavigate();
+
+  const handleBack = () => {
+    navigate(-1);
+  };
+
+  const goToLogin = () => {
+    navigate('/login');
+  };
+
   return (
     <S.Wrapper>
-      <ChevronLeft size={28} />
+      <ChevronLeft size={28} onClick={handleBack} style={{ cursor: 'pointer' }} />
       <S.Title>선물하기</S.Title>
-      <UserRound size={25} />
+      <UserRound size={25} onClick={goToLogin} style={{ cursor: 'pointer' }} />
     </S.Wrapper>
   );
 };
