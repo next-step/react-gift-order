f16d2d20324aad893c5b6cc5624bc8009ae981d1
Ajin <ajin@Ajinui-noteubug.local>
2025-07-01 03:43:34 +0900

    feat: /login 경로에서 로그인 페이지 렌더링 및 NotFound 라우팅

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,26 @@ import { createRoot } from 'react-dom/client';
 import App from '@/App.tsx';
 import { ThemeProvider } from '@emotion/react';
 import { theme } from '@/styles/theme';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import LoginPage from '@/pages/LoginPage.tsx';
+import NotFoundPage from '@/pages/NotFoundPage.tsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />, // 기존 UI
+    errorElement: <NotFoundPage />,
+  },
+  {
+    path: '/login',
+    element: <LoginPage />,
+  },
+]);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ThemeProvider theme={theme}>
-      <App />
+      <RouterProvider router={router} />
     </ThemeProvider>
   </StrictMode>
 );
---
+++ src/pages/LoginPage.tsx
@@ -0,0 +1,10 @@
+const LoginPage = () => {
+  return (
+    <div style={{ padding: '2rem', textAlign: 'center' }}>
+      <h1>로그인 페이지입니다</h1>
+      <p>여기에 로그인 UI!</p>
+    </div>
+  );
+};
+
+export default LoginPage;
