7bc8e63042adb215c86a6af6f9310350517cfa7a
LeeHeeSung98989898 <goqpqp234@gmail.com>
2025-07-01 18:45:11 +0900

    [충남대학교 FE 11팀 이희성]  0단계 - 기본 코드 준비 (#12)
    * feat: 2단계 step0 초기 세팅
    
    * docs: README 초기화
    
    * feat: 이전 과제 3단계 피드백 [제안] 반영(코드 중복 제거, 의미를 담은 숫자 constant선언

---
+++ src/LoginPage.tsx
@@ -0,0 +1,50 @@
+import { useNavigate } from "react-router";
+import styled from "@emotion/styled";
+import KakaoLogo from "@/assets/kakaologo.svg";
+import PageContainer from "@/components/PageContainer";
+import Input from "@/components/Input";
+
+const LogoImage = styled.img`
+  width: 88px;
+  height: auto;
+  margin-bottom: ${({ theme }) => theme.spacing.spacing6};
+`;
+
+const Button = styled.button`
+  ${({ theme }) => theme.typography.body.body2Regular};
+  width: 100%;
+  max-width: 320px;
+  padding: 12px;
+  background-color: ${({ theme }) => theme.color.semantic.kakaoYellow};
+  color: #000;
+  border: none;
+  border-radius: 6px;
+  margin-top: ${({ theme }) => theme.spacing.spacing9};
+  cursor: pointer;
+
+  &:active {
+    background-color: ${({ theme }) => theme.color.semantic.kakaoYellowPressed};
+    border: none;
+  }
+`;
+
+export default function LoginPage() {
+  const navigate = useNavigate();
+
+  const handleLogin = () => {
+    if(window.history.length > 2) {
+      navigate(-1);
+    } else {
+      navigate("/");
+    }
+  };
+
+  return (
+    <PageContainer>
+      <LogoImage src={KakaoLogo} alt="kakao logo" />
+      <Input type="email" placeholder="이메일" />
+      <Input type="password" placeholder="비밀번호" />
+      <Button onClick={handleLogin}>로그인</Button>
+    </PageContainer>
+  );
+}
