8fff720f84cf9541e7e8a4066e26b478eb6c94be
kanghaeun <xgkg0330@jnu.ac.kr>
2025-07-02 03:25:44 +0900

    feat: 이메일 입력값 유효성 검사 및 에러 메시지 추가

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -4,26 +4,43 @@ import { useNavigate } from 'react-router-dom';
 import NavigationBar from '@/common/NavigationBar';
 import Input from '@/common/Input';
 import LoginButton from '@/components/login/LoginButton';
+import { useState } from 'react';
 
 const LoginForm = () => {
   const { form, handleChange } = useLoginForm();
+  const [emailError, setEmailError] = useState<string | null>(null);
   const navigate = useNavigate();
 
   const loginRedirect = () => {
     navigate('/');
   };
 
+  const isValidEmail = (email: string): boolean => {
+    return email.includes('@') && email.includes('.');
+  };
+
+  const handleEmailBlur = () => {
+    if (form.id.trim() === '') setEmailError('ID를 입력해주세요.');
+    else if (!isValidEmail(form.id))
+      setEmailError('ID는 이메일 형식으로 입력해주세요.');
+    else setEmailError(null);
+  };
+
   return (
     <Layout>
       <NavigationBar />
       <Logo>kakao</Logo>
-
-      <Input
-        name="id"
-        placeholder="이메일"
-        value={form.id}
-        onChange={handleChange}
-      />
+      <FormWrapper>
+        <Input
+          name="id"
+          placeholder="이메일"
+          value={form.id}
+          onChange={handleChange}
+          onBlur={handleEmailBlur}
+          hasError={!!emailError}
+        />
+        {emailError && <ErrorText>{emailError}</ErrorText>}
+      </FormWrapper>
       <Input
         name="password"
         type="password"
@@ -51,4 +68,17 @@ const Layout = styled.div`
   width: 100%;
   height: 100vh;
   gap: ${({ theme }) => theme.spacing.spacing2};
+`;
+const FormWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  max-width: 388px;
+  width: 100%;
+`;
+const ErrorText = styled.div`
+  color: ${({ theme }) => theme.colors.red700};
+  font-size: ${({ theme }) => theme.typography.fontSizes.label2};
+  margin-top: -12px;
+  margin-bottom: 12px;
 `;
