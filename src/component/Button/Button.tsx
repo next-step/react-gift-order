b75839fc9356749621420da31e35bf443409f828
daaoooy <y020d226y@daum.net>
2025-07-02 16:14:44 +0900

    feat(button) : ID, PW 조건 모두 만족해야 버튼이 활성화되는 기능 추가
    - activeBackground, inactiveBackground (color 도 동일하게) 을
      이용하여 버튼 활성화 기능을 구현하였습니다.
    - disable 값을 통해서 참이면 inactive, 거짓이면 active 로 되도록
      해주었습니다.
    - 커서도 동일하게 disable을 통해 active일 때만 커서가 허용되도록
      해주었습니다.

--- src/component/Button/Button.tsx
+++ src/component/Button/Button.tsx
@@ -2,7 +2,7 @@ import type { ComponentPropsWithoutRef } from 'react'
 import { theme } from '@/theme'
 import styled from '@emotion/styled'
 
-type ButtonType = 'login' | 'More' | 'home'
+type ButtonType = 'login' | 'more' | 'home'
 
 interface ButtonProps extends ComponentPropsWithoutRef<'button'> {
   buttonType: ButtonType
@@ -13,18 +13,22 @@ const buttonStyleByType = {
     width: '100%',
     height: theme.spacing[11],
     margin: `${theme.spacing[8]} 0 0 0`,
-    background: theme.colors.semanticColor.brandColor.kakaoYellow,
-    color: theme.colors.semanticColor.textColor.default,
+    activeBackground: theme.colors.semanticColor.brandColor.kakaoYellow,
+    inactiveBackground: theme.colors.colorScale.yellow[300],
+    activeColor: theme.colors.semanticColor.textColor.default,
+    inactiveColor: theme.colors.colorScale.gray[700],
     typography: theme.typography.label2Regular,
     hoverBackground: theme.colors.semanticColor.brandColor.kakaoYellowHover,
     border: 'none',
   },
-  More: {
+  more: {
     width: '70%',
     height: `${theme.spacing[10]}`,
     margin: `${theme.spacing[4]} auto`,
-    background: theme.colors.colorScale.gray[0],
-    color: theme.colors.semanticColor.textColor.default,
+    activeBackground: theme.colors.colorScale.gray[0],
+    inactiveBackground: theme.colors.colorScale.gray[0],
+    activeColor: theme.colors.semanticColor.textColor.default,
+    inactiveColor: theme.colors.semanticColor.textColor.default,
     typography: theme.typography.label1Regular,
     hoverBackground: theme.colors.colorScale.gray[100],
     border: `1px solid ${theme.colors.colorScale.gray[400]}`,
@@ -33,31 +37,44 @@ const buttonStyleByType = {
     width: '160px',
     height: theme.spacing[11],
     margin: theme.spacing[10],
-    background: theme.colors.semanticColor.brandColor.kakaoYellow,
-    color: theme.colors.semanticColor.textColor.default,
+    activeBackground: theme.colors.semanticColor.brandColor.kakaoYellow,
+    inactiveBackground: theme.colors.semanticColor.brandColor.kakaoYellow,
+    activeColor: theme.colors.semanticColor.textColor.default,
+    inactiveColor: theme.colors.semanticColor.textColor.default,
     typography: theme.typography.label1Regular,
     hoverBackground: theme.colors.semanticColor.brandColor.kakaoYellowHover,
     border: 'none',
   },
 }
 
-const StyledButton = styled.button<{ $buttonType: ButtonType }>`
+const StyledButton = styled.button<{
+  $buttonType: ButtonType
+}>`
   display: block;
-  cursor: pointer;
   border-radius: ${theme.spacing[1]};
   width: ${({ $buttonType }) => buttonStyleByType[$buttonType].width};
   height: ${({ $buttonType }) => buttonStyleByType[$buttonType].height};
   margin: ${({ $buttonType }) => buttonStyleByType[$buttonType].margin};
-  background-color: ${({ $buttonType }) =>
-    buttonStyleByType[$buttonType].background};
-  color: ${({ $buttonType }) => buttonStyleByType[$buttonType].color};
   border: ${({ $buttonType }) => buttonStyleByType[$buttonType].border};
-
   ${({ $buttonType }) => buttonStyleByType[$buttonType].typography};
 
+  cursor: ${({ disabled }) => (disabled ? 'not-allowed' : 'pointer')};
+
+  background-color: ${({ disabled, $buttonType }) =>
+    disabled
+      ? buttonStyleByType[$buttonType].inactiveBackground
+      : buttonStyleByType[$buttonType].activeBackground};
+
+  color: ${({ disabled, $buttonType }) =>
+    disabled
+      ? buttonStyleByType[$buttonType].inactiveColor
+      : buttonStyleByType[$buttonType].activeColor};
+
   &:hover {
-    background-color: ${({ $buttonType }) =>
-      buttonStyleByType[$buttonType].hoverBackground};
+    background-color: ${({ disabled, $buttonType }) =>
+      disabled
+        ? buttonStyleByType[$buttonType].inactiveBackground
+        : buttonStyleByType[$buttonType].hoverBackground};
   }
 `
 
