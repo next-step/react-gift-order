adf4c7f321b3fb8a60d8c98b6169bdbe1fd5a35b
sumin <ssumin512@gmail.com>
2025-07-02 15:53:05 +0900

    커스텀 훅 적용

--- src/Components/Login/LoginForm.tsx
+++ src/Components/Login/LoginForm.tsx
@@ -1,7 +1,7 @@
 import styled from '@emotion/styled';
 import LogoImg from '@/Assets/icons/logo.png';
 import LoginButton from '@/Components/Login/LoginButton';
-
+import { useLoginForm } from '@/Hooks/useLoginForm';
 
 const Container = styled.div`
     width: 100%;
@@ -35,14 +35,45 @@ const PwInput = styled.input`
     border-bottom: 1px solid ${({ theme }) => theme.color.semantic.border.default};
     font-size: 1rem;
 `
+const ErrorMessage = styled.div`
+    color:red;
+    font-size: 0.875rem;
+    margin-top: 0.5rem;
+`;
 
 const LoginForm = () => {
+    const {
+        email,
+        setEmail,
+        password,
+        setPassword,
+        emailError,
+        passwordError,
+        isValid,
+        handleEmailBlur,
+        handlePasswordBlur
+    } = useLoginForm();
+
     return (
         <Container>
             <Logo src={LogoImg} alt="로고" />
-            <EmailInput type="email" placeholder="이메일" />
-            <PwInput type="password" placeholder="비밀번호" />
-            <LoginButton />
+            <EmailInput
+                type="email"
+                placeholder="이메일"
+                value={email}
+                onChange={e => setEmail(e.target.value)}
+                onBlur={handleEmailBlur}
+            />
+            {emailError && <ErrorMessage>{emailError}</ErrorMessage>}
+            <PwInput
+                type="password"
+                placeholder="비밀번호"
+                value={password}
+                onChange={e => setPassword(e.target.value)}
+                onBlur={handlePasswordBlur}
+            />
+            {passwordError && <ErrorMessage>{passwordError}</ErrorMessage>}
+            <LoginButton/>
         </Container>
     )
 }
