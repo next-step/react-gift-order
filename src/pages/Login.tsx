e581b221788e3fb30e9646ab0c89ce4e00ece94d
HanseKim <kimbird2002@naver.com>
2025-07-02 15:22:46 +0900

    feat: 로그인 로직 useState 사용해서 완성

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,9 +1,13 @@
+import { useState } from 'react';
 import {
   LoginTitle,
   LoginButton,
   LoginContainer,
   LoginInput,
   LoginPasswordInput,
+  LoginForm,
+  IDErrorContainer,
+  PWErrorContainer,
 } from '@/styles/Login.styles';
 
 type LoginProps = {
@@ -11,12 +15,76 @@ type LoginProps = {
 };
 
 function Login({ onLogin }: LoginProps) {
+  const [id, setId] = useState<string>('');
+  const [pw, setPw] = useState<string>('');
+  const [idError, setIdError] = useState<string>('');
+  const [pwError, setPwError] = useState<string>('');
+  function isEmail(value: string) {
+    return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(value);
+  }
+
+  function checkId(id: string): boolean {
+    if (!id) {
+      setIdError('ID를 입력해주세요');
+      return false;
+    } else if (!isEmail(id)) {
+      setIdError('ID는 이메일 형식으로 입력해주세요.');
+      return false;
+    }
+    setIdError('');
+    return true;
+  }
+  function checkPw(pw: string): boolean {
+    if (!pw) {
+      setPwError('PW를 입력해주세요.');
+      return false;
+    } else if (pw.length < 8) {
+      setPwError('PW는 최소 8글자 이상이어야 합니다.');
+      return false;
+    }
+    setPwError('');
+    return true;
+  }
+
+  function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+
+    if (!checkId(id)) {
+      return;
+    }
+    if (!checkPw(pw)) {
+      return;
+    }
+
+    onLogin();
+  }
   return (
     <LoginContainer>
       <LoginTitle>KAKAO</LoginTitle>
-      <LoginInput placeholder="이메일" />
-      <LoginPasswordInput type="password" placeholder="비밀번호" />
-      <LoginButton onClick={onLogin}>로그인</LoginButton>
+      <LoginForm onSubmit={handleSubmit}>
+        <LoginInput
+          placeholder="이메일"
+          value={id}
+          onChange={(e) => setId(e.target.value)}
+          onBlur={(e) => {
+            checkId(e.target.value);
+          }}
+        />
+        {idError === '' ? <></> : <IDErrorContainer>{idError}</IDErrorContainer>}
+        <LoginPasswordInput
+          type="password"
+          placeholder="비밀번호"
+          value={pw}
+          onChange={(e) => setPw(e.target.value)}
+          onBlur={(e) => {
+            checkPw(e.target.value);
+          }}
+        />
+        {pwError === '' ? <></> : <PWErrorContainer>{pwError}</PWErrorContainer>}
+        <LoginButton type="submit" active={idError == '' && pwError === ''}>
+          로그인
+        </LoginButton>
+      </LoginForm>
     </LoginContainer>
   );
 }
