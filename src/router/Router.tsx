77da09316c53f0cb41fd290dd67290ffad903f58
Moderator11 <p7925221@gmail.com>
2025-07-02 14:56:47 +0900

    chore: centralize route paths into a PATH object

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -4,14 +4,16 @@ import LoginPage from "@src/pages/LoginPage";
 import Mainpage from "@src/pages/MainPage";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
+const PATH = { MAIN: "/", LOGIN: "/login", EXCEPT: "*" } as const;
+
 function Router() {
   return (
     <BrowserRouter>
       <NavBar />
       <Routes>
-        <Route path="/" element={<Mainpage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="*" element={<ErrorPage />} />
+        <Route path={PATH.MAIN} element={<Mainpage />} />
+        <Route path={PATH.LOGIN} element={<LoginPage />} />
+        <Route path={PATH.EXCEPT} element={<ErrorPage />} />
       </Routes>
     </BrowserRouter>
   );
