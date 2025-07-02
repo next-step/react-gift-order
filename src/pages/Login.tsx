b84b89523e516d741705c354db6f23779c878f98
hyemin <hyemin6647@naver.com>
2025-07-02 17:16:14 +0900

    feat: 로그인 기능 구현(useInput 사용)

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,4 +1,3 @@
-
 import Navbar from "./../components/navbar/Navbar";
 import { PaddingSm } from "./../components/padding/Padding";
 import { PaddingMd } from "@/components/padding/Padding";
@@ -56,7 +55,6 @@ const LoginBtn = styled.button<{ activated: boolean }>`
   opacity: ${({ activated }) => (activated ? 1 : 0.5)};
   cursor: ${({ activated }) => (activated ? "pointer" : "not-allowed")};
 `;
-
 const ValidationMsg = styled.p`
   color: ${({ theme }) => theme.colors.red.red700};
   ${({ theme }) => theme.typography.label2Regular}
