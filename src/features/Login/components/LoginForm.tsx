815cdfa7f192dcb9a1004b8a0ff887b190ff5370
daaoooy <y020d226y@daum.net>
2025-07-02 16:29:30 +0900

    feat(login) : custom hook을 사용하여 로그인 기능 및 UI 구현
    - 구현한 커스텀 훅 useLoginForm을 이용해 로그인 기능을 구현했습니다.
    - 기존 UI에 에러 메세지가 나올 수 있도록 추가하였습니다.
    - 에러 메세지는 에러가 있을 땐 보이도록, 에러가 없을 땐 보이지 않게(공간 차지X) 해주었습니다.
    - 그 외에 세세한 UI 를 수정하였습니다 (input form 밑 보더 색상 변경,
      마진 조정 등)

--- src/features/Login/components/LoginForm.tsx
+++ src/features/Login/components/LoginForm.tsx
@@ -3,17 +3,40 @@ import {
   FormContainer,
   KakaoTitle,
   InputForm,
+  ErrorMessage,
 } from './LoginForm.styles'
 import { useNavigate, useLocation } from 'react-router-dom'
 import KaKaoTitleIcon from '@/assets/icons/kakao-title.svg?react'
 import MyButton from '@/component/Button/Button'
+import { useLoginForm } from '../hooks/useLoginForm'
 
-const Input: React.FC = () => {
+const LoginForm: React.FC = () => {
+  const {
+    email,
+    setEmail,
+    password,
+    setPassword,
+    emailError,
+    passwordError,
+    checkEmail,
+    checkPassword,
+    loginOK,
+    emailTouched,
+    setEmailTouched,
+    passwordTouched,
+    setPasswordTouched,
+  } = useLoginForm()
+
   const navigate = useNavigate()
   const location = useLocation()
   const hasPreviousPage = location.key !== 'default'
 
   const handleLoginClick = () => {
+    const emailValid = checkEmail()
+    const passwordValid = checkPassword()
+    if (!emailValid || !passwordValid) return
+    console.log('로그인 요청:', { email, password })
+
     if (hasPreviousPage) {
       navigate(-1)
     } else {
@@ -27,9 +50,37 @@ const Input: React.FC = () => {
         <KakaoTitle>
           <KaKaoTitleIcon />
         </KakaoTitle>
-        <InputForm placeholder="이메일" type="email" />
-        <InputForm placeholder="비밀번호" type="password" />
-        <MyButton buttonType="login" onClick={handleLoginClick}>
+
+        <InputForm
+          placeholder="이메일"
+          type="email"
+          value={email}
+          onChange={(e) => {
+            setEmail(e.target.value)
+          }}
+          onBlur={() => setEmailTouched(true)}
+          isError={!!emailError}
+        />
+        <ErrorMessage isActive={!!emailError}>{emailError}</ErrorMessage>
+
+        <InputForm
+          placeholder="비밀번호"
+          type="password"
+          value={password}
+          onChange={(e) => {
+            setPassword(e.target.value)
+            checkPassword()
+          }}
+          onBlur={() => setPasswordTouched(true)}
+          isError={!!passwordError}
+        />
+        <ErrorMessage isActive={!!passwordError}>{passwordError}</ErrorMessage>
+
+        <MyButton
+          buttonType="login"
+          onClick={handleLoginClick}
+          disabled={!loginOK()}
+        >
           로그인
         </MyButton>
       </FormContainer>
@@ -37,4 +88,4 @@ const Input: React.FC = () => {
   )
 }
 
-export default Input
+export default LoginForm
