59e62f511bea7756e1ae94b16d31a755ccb2080e
jskim <jskim3936@naver.com>
2025-07-02 15:52:45 +0900

    feat(Login): ID inputBox 조건에 맞게 구현

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -4,6 +4,7 @@ import KakaoIconUrl from '../assets/KaKaoLogo.svg';
 import GlobalStyle from '@/styles/global';
 import NavigationBar from '@components/NavigationBar';
 import { useLocation, useNavigate } from 'react-router-dom';
+import { useState } from 'react';
 
 const Wrapper = styled.div`
   width: 100%;
@@ -45,7 +46,7 @@ const InputSection = styled.section`
   padding: 16px;
 `;
 
-const InputBox = styled.input`
+const InputBox = styled.input<{ hasError?: boolean }>`
   width: 100%;
   box-sizing: border-box;
   color: rgb(42, 48, 56);
@@ -60,7 +61,7 @@ const InputBox = styled.input`
   border-color: rgb(220, 222, 227);
   :focus {
     outline: none;
-    border-color: rgb(134, 139, 148);
+    border-color: ${(props) => (props.hasError ? 'rgb(250, 52, 44)' : 'rgb(134, 139, 148)')};
   }
   ::placeholder {
     color: #b0b3ba;
@@ -92,6 +93,22 @@ const Login: React.FC = () => {
 
   const redirectTo = state?.from?.pathname ?? '/';
 
+  // state 생성
+  const [id, setId] = useState('');
+  const [idError, setIdError] = useState('');
+
+  // ID 조건 검사
+  const validateId = (value: string) => {
+    if (!value.trim()) {
+      return 'ID를 입력해주세요.';
+    }
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(value)) {
+      return 'ID는 이메일 형식으로 입력해주세요.';
+    }
+    return '';
+  };
+
   const handleClick = () => {
     navigate(redirectTo, { replace: true });
   };
@@ -105,7 +122,42 @@ const Login: React.FC = () => {
             <Logo src={KakaoIconUrl} alt="카카오 로고"></Logo>
             <InputSection>
               <div>
-                <InputBox placeholder="이메일"></InputBox>
+                <InputBox
+                  placeholder="이메일"
+                  value={id}
+                  hasError={!!idError}
+                  onChange={(e) => {
+                    const value = e.target.value;
+                    setId(value);
+
+                    const error = validateId(value);
+                    setIdError(error);
+                  }}
+                  onFocus={(e) => {
+                    e.currentTarget.style.borderColor = 'rgb(134, 139, 148)';
+                  }}
+                  onBlur={(e) => {
+                    const err = validateId(e.target.value);
+                    setIdError(err);
+                    // 오류가 있으면 빨간색 테두리 적용
+                    if (err) {
+                      e.currentTarget.style.borderColor = 'red';
+                    }
+                  }}
+                  style={idError ? { borderColor: 'rgb(250, 52, 44)' } : {}}
+                />
+                {idError && (
+                  <span
+                    css={css`
+                      color: rgb(250, 52, 44);
+                      font-size: 0.75rem;
+                      margin-top: 4px;
+                      display: inline-block;
+                    `}
+                  >
+                    {idError}
+                  </span>
+                )}
               </div>
               <div
                 css={css`
@@ -115,7 +167,7 @@ const Login: React.FC = () => {
                 `}
               />
               <div>
-                <InputBox placeholder="비밀번호"></InputBox>
+                <InputBox placeholder="비밀번호" type="password" />
               </div>
               <div
                 css={css`
