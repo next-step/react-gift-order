bfddc0bcab0dcdd9dd0c1d26ab5efe3cdda50b98
kimgho <kgh67811@knu.ac.kr>
2025-07-03 14:35:31 +0900

    feat(card-template): 카드 템플릿 목 데이터 추가 및 타입 정의

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,2 +1,3 @@
 export { type CategoryType } from "@/types/category-type";
 export { type ProductType } from "@/types/product-type";
+export { type CardTemplateType } from "@/types/card-template-type";
