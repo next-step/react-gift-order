26e5ab59679f32cd7065ac50f1aa494befc2f250
aaaaaattt <choigur03@gmail.com>
2025-07-04 01:52:09 +0900

    chore: components 내부의 .js 파일 삭제

--- src/components/NotFound.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var jsx_runtime_1 = require("@emotion/react/jsx-runtime");
-var NotFound = function () {
-    return (0, jsx_runtime_1.jsx)("div", {});
-};
-exports.default = NotFound;
