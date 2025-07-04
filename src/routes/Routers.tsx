3ad3b92a46520b25beae09de884307738cffd069
Changmok LEE <cmlee5075@gmail.com>
2025-07-04 19:54:28 +0900

    feat: route path 상수화

--- src/routes/Routers.tsx
+++ src/routes/Routers.tsx
@@ -2,13 +2,14 @@ import GiftsPage from '@/page/GiftPage';
 import LoginPage from '@/page/LoginPage';
 import NotFound from '@/page/NotFound';
 import { Route, Routes } from 'react-router-dom';
+import { ROUTES } from './routes';
 
 const AppRouter = () => {
   return (
     <Routes>
-      <Route path="/" element={<GiftsPage />} />
-      <Route path="/login" element={<LoginPage />} />
-      <Route path="*" element={<NotFound />} />
+      <Route path={ROUTES.HOME} element={<GiftsPage />} />
+      <Route path={ROUTES.LOGIN} element={<LoginPage />} />
+      <Route path={ROUTES.NOTFOUND} element={<NotFound />} />
     </Routes>
   );
 };
