65aeeffe082acdaa54fe28d1b052af9861f49853
toothlessdev <toothless042@gmail.com>
2025-07-04 19:36:51 +0900

    feature: AuthContext 및 AuthProvider 구현

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,17 @@ import { RouterProvider } from "react-router-dom";
 
 import { theme } from "@/app/theme";
 
+import { AuthProvider } from "@/features/auth/context/AuthContext";
+
 import { browserRouter } from "@/Router";
 import { ThemeProvider } from "@emotion/react";
 
 export default function App() {
     return (
         <ThemeProvider theme={theme}>
-            <RouterProvider router={browserRouter} />
+            <AuthProvider>
+                <RouterProvider router={browserRouter} />
+            </AuthProvider>
         </ThemeProvider>
     );
 }
