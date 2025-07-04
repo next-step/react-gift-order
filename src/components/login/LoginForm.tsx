a0d425dbbbfd528863f3952b9bd8db1c913e8b67
jiwonWP <bread0264@gmail.com>
2025-07-04 16:15:58 +0900

    refactor: LoginForm 스타일 분리 및 스타일 컴포넌트 모듈화

--- src/components/login/LoginForm.tsx
+++ src/components/login/LoginForm.tsx
@@ -1,7 +1,12 @@
 /** @jsxImportSource @emotion/react */
-import styled from "@emotion/styled";
 import { useState } from "react";
-
+import {
+  FormSection,
+  InputWrapper,
+  Input,
+  ErrorMessage,
+  LoginButton,
+} from "./LoginFormStyles";
 type Props = {
   onLoginSuccess: (email: string) => void;
 };
@@ -89,65 +94,3 @@ export const LoginForm = ({ onLoginSuccess }: Props) => {
     </FormSection>
   );
 };
-
-const FormSection = styled.section`
-  width: 100%;
-  max-width: 28rem;
-  padding: ${({ theme }) => theme.spacing.spacing4};
-`;
-
-const InputWrapper = styled.div`
-  width: 100%;
-  margin-bottom: ${({ theme }) => theme.spacing.spacing4};
-`;
-
-type InputProps = {
-  isError?: boolean;
-};
-
-const Input = styled.input<InputProps>`
-  width: 100%;
-  box-sizing: border-box;
-  color: ${({ theme }) => theme.colors.textDefault};
-  transition: border-color 200ms;
-  border: none;
-  border-bottom: 1px solid
-    ${({ theme, isError }) =>
-      isError ? theme.colors.red700 : theme.colors.borderDefault};
-  min-height: 3rem;
-  ${({ theme }) => theme.typography.body1Regular};
-  padding: ${({ theme }) => `${theme.spacing.spacing2} 0`};
-
-  &::placeholder {
-    color: ${({ theme }) => theme.colors.textPlaceholder};
-  }
-`;
-
-const ErrorMessage = styled.p`
-  ${({ theme }) => theme.typography.body1Regular};
-  color: ${({ theme }) => theme.colors.red700};
-  margin-top: ${({ theme }) => theme.spacing.spacing1};
-`;
-
-const LoginButton = styled.button<{ disabled: boolean }>`
-  width: 100%;
-  height: 50px;
-  margin-top: ${({ theme }) => theme.spacing.spacing4};
-  ${({ theme }) => theme.typography.subtitle2Regular};
-
-  color: ${({ theme }) => theme.colors.textDefault};
-  background-color: ${({ theme }) => theme.colors.kakaoYellow};
-  border: none;
-  border-radius: 5px;
-  cursor: ${({ disabled }) => (disabled ? "not-allowed" : "pointer")};
-  opacity: ${({ disabled }) => (disabled ? 0.5 : 1)};
-  transition: background-color 200ms;
-
-  &:hover {
-    background-color: ${({ theme }) => theme.colors.kakaoYellowHover};
-  }
-
-  &:active {
-    background-color: ${({ theme }) => theme.colors.kakaoYellowActive};
-  }
-`;
