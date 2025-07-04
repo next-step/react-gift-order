b525ea8ce8efb997a18249f5a6a9eb5e698256fc
toothlessdev <toothless042@gmail.com>
2025-07-04 19:46:25 +0900

    feature: MyPage 에 AuthGuard 적용

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,6 +1,9 @@
 import { createBrowserRouter, createRoutesFromElements, Route } from "react-router-dom";
 
+import { AuthGuard } from "@/features/auth/guards/AuthGuard";
+
 import HomePage from "@/pages/HomePage";
+import MyPage from "@/pages/MyPage";
 import NotFoundPage from "@/pages/NotFoundPage";
 import { AuthLayout } from "@/pages/auth/AuthLayout";
 import SignInPage from "@/pages/auth/SignInPage";
@@ -13,6 +16,14 @@ const router = createRoutesFromElements(
         <Route path="/auth" element={<AuthLayout />}>
             <Route path="signin" element={<SignInPage />} />
         </Route>
+        <Route
+            path="my"
+            element={
+                <AuthGuard>
+                    <MyPage />
+                </AuthGuard>
+            }
+        />
         <Route path="*" element={<NotFoundPage />} />
     </Route>,
 );
