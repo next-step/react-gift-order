82018c64fd1eb88ecb69647a2c3767b862de832d
toothlessdev <toothless042@gmail.com>
2025-07-04 18:03:34 +0900

    refactor(1단계 피드백 반영사항): 불필요한 Fragment 제거

--- src/shared/ui/Input.tsx
+++ src/shared/ui/Input.tsx
@@ -1,4 +1,4 @@
-import { forwardRef, Fragment } from "react";
+import { forwardRef } from "react";
 
 import * as Styles from "@/shared/ui/Input.styled";
 
@@ -8,9 +8,9 @@ export interface InputProps extends React.ComponentProps<"input"> {
 
 export const Input = forwardRef<HTMLInputElement, InputProps>(({ error, ...props }, ref) => {
     return (
-        <Fragment>
+        <>
             <Styles.InputElement type="text" ref={ref} error={error} {...props} />
             <Styles.InputErrorMessage>{error}</Styles.InputErrorMessage>
-        </Fragment>
+        </>
     );
 });
