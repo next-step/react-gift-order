16e571ff007292aa6671ac2a66e5dbe0b474ecec
eunchae-04 <rhdmsco1163@naver.com>
2025-07-04 15:45:50 +0900

    fix: 중복된 UserManagementProvider 제거로 로그인 후 이동 문제 해결

--- src/pages/Login/LoginPage.tsx
+++ src/pages/Login/LoginPage.tsx
@@ -2,17 +2,14 @@
 import { useTheme } from '@emotion/react';
 import LoginFormSection from './LoginFormSection';
 import { backgroundStyle } from './Login.style';
-import { UserManagementProvider } from './userManagement';
 
 const LoginPage = () => {
   const theme = useTheme();
 
   return (
-    <UserManagementProvider>
-      <div css={backgroundStyle(theme)}>
-        <LoginFormSection />
-      </div>
-    </UserManagementProvider>
+    <div css={backgroundStyle(theme)}>
+      <LoginFormSection />
+    </div>
   );
 };
 
