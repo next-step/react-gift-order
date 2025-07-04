eb6313f0098ad99713eba73e59ed922d1c808c98
jiwonWP <bread0264@gmail.com>
2025-07-04 14:38:40 +0900

    feat: AuthContext 생성 및 로그인 상태 관리 로직 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,25 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { GlobalResetStyle } from './styles/reset';
-import { Global } from '@emotion/react';
-import { ThemeProvider } from '@emotion/react';
-import { theme } from './styles/theme/theme';
-import GiftMain from './pages/GiftMain';
-import Login from './pages/Login';
-import NotFound from './pages/NotFound';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { GlobalResetStyle } from "./styles/reset";
+import { Global } from "@emotion/react";
+import { ThemeProvider } from "@emotion/react";
+import { theme } from "./styles/theme/theme";
+import { AuthProvider } from "./contexts/AuthContext";
+import GiftMain from "./pages/GiftMain";
+import Login from "./pages/Login";
+import NotFound from "./pages/NotFound";
 
 const App = () => {
   return (
     <ThemeProvider theme={theme}>
       <Global styles={GlobalResetStyle} />
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<GiftMain />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
+        <AuthProvider>
+          <Routes>
+            <Route path="/" element={<GiftMain />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </AuthProvider>
       </BrowserRouter>
     </ThemeProvider>
   );
