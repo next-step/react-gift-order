4ad2e53b7a458452496a5bf826b4887027ef5a86
DowanKim <83197376+dowankim1024@users.noreply.github.com>
2025-07-04 09:02:02 +0900

    feat: step1 로그인 기능 구현 (#67)
    * feat: 로그인 유효성 검사 커스텀 훅 구현
    
    * feat: 로그인 유효성 검사 커스텀 훅 로그인 페이지에  적용
    
    * style: 에러메세지 color 및 정렬을 위한 레이아웃 구조 수정
    
    * feat: Input 밑줄 포커스 시 검정, 포커스 아웃 시 에러상태에 따라 밑줄 색상분기 처리
    
    * feat: 로그인 유효성 조건 만족 시에만 버튼 활성화 구현
    
    * Update README.md
    
    * Update README.md
    
    * refactor: 상수화 및 분리 & 중복 타입 명시 제거
    
    * refactor: invalid 선언시 함수 중복호출 제거 & 사용성 증가를 위한 Blur함수  제거

--- src/pages/LoginPage/index.tsx
+++ src/pages/LoginPage/index.tsx
@@ -1,29 +1,48 @@
 import { useNavigate, useLocation } from 'react-router-dom';
 import NavigationBar from '@/components/NavigationBar';
-import {Container, Logo, Input, LoginButton} from './styles'
+import { Container, Logo, Input, LoginButton, ErrorMessage, InputWrapper } from './styles'
 import LogoIcon from '@/assets/logo.svg';
+import { useLoginForm } from './useLoginForm';
+import { PATH } from '../../constants/paths';
 
 interface LocationState {
   from?: string;
 }
 const LoginPage = () => {
   const navigate = useNavigate();
-  const location = useLocation();
-  const state = location.state as LocationState;
-  const from = state?.from || '/';
+  const { state } = useLocation<LocationState>();
+  const from = state?.from || PATH.HOME;
+  const {
+    email,
+    pw,
+    emailError,
+    pwError,
+    emailTouched,
+    pwTouched,
+    isValid,
+    handleEmailChange,
+    handlePwChange,
+  } = useLoginForm();
 
   const handleLogin = () => {
-    navigate(from, { replace: true });
+    if (isValid) navigate(from, { replace: true });
   };
+  
 
   return (
     <>
       <NavigationBar />
       <Container>
-        <Logo src={LogoIcon} alt='kakao logo'/>
-        <Input type="email" name="email" id="email" autoComplete="email" placeholder="이메일" />
-        <Input type="password" name="password" id="password" autoComplete="current-password" placeholder="비밀번호" />
-        <LoginButton onClick={handleLogin}>로그인</LoginButton>
+        <Logo src={LogoIcon} alt='kakao logo' />
+        <InputWrapper>
+          <Input type="email" name="email" id="email" autoComplete="email" placeholder="이메일" value={email} onChange={e => handleEmailChange(e.currentTarget.value)} isError={Boolean(emailError)} />
+          {emailError && <ErrorMessage>{emailError}</ErrorMessage>}
+        </InputWrapper>
+        <InputWrapper>
+          <Input type="password" name="password" id="password" autoComplete="current-password" placeholder="비밀번호" value={pw} onChange={e => handlePwChange(e.currentTarget.value)} isError={Boolean(pwError)}/>
+          {pwError && <ErrorMessage>{pwError}</ErrorMessage>}
+        </InputWrapper>
+        <LoginButton disabled={!(isValid && emailTouched && pwTouched)} onClick={handleLogin}>로그인</LoginButton>
       </Container>
     </>
   );
--- src/pages/LoginPage/styles.ts
+++ src/pages/LoginPage/styles.ts
@@ -12,14 +12,29 @@ export const Logo = styled.img`
     width: 120px;
     height: auto;
 `;
+export const ErrorMessage = styled.p`
+  font: ${({ theme }) => theme.typography.label2Regular};
+  color: ${({ theme }) => theme.colors.semantic.critical};
+  margin-top: -${({ theme }) => theme.spacing.spacing4};
+`;
 
-export const Input = styled.input`
+export const InputWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  width: 360px;
+  margin-bottom: ${({ theme }) => theme.spacing.spacing6};
+`;
+
+export const Input = styled.input<{ isError: boolean }>`
   width: 360px;
   padding: ${({ theme }) => theme.spacing.spacing3};
   margin-bottom: ${({ theme }) => theme.spacing.spacing6};
   font : ${({ theme }) => theme.typography.body2Regular};
   border: none;
-  border-bottom: 1px solid ${({ theme }) => theme.colors.semantic.borderDefault};
+  border-bottom: 1px solid
+    ${({ isError, theme }) =>
+    isError ? theme.colors.semantic.critical : theme.colors.semantic.borderDefault};
   background: transparent;
   outline: none;
   color: ${({ theme }) => theme.colors.semantic.textDefault};
@@ -44,4 +59,9 @@ export const LoginButton = styled.button`
   border-radius: ${({ theme }) => theme.spacing.spacing1};
   cursor: pointer;
   margin-top: ${({ theme }) => theme.spacing.spacing10};
+  &:disabled {
+    background-color: ${({ theme }) => theme.colors.semantic.backgroundDisabled};;
+    cursor: not-allowed;
+    color: ${({ theme }) => theme.colors.semantic.textDisabled};;
+  }
 `;
