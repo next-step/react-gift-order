62b3fc3f13be46fd46e729647af248ad604a2560
hojunlee <leehojun.full@gmail.com>
2025-07-06 07:11:53 +0900

    style : recipientsModalContainer margin and padding basic correction

--- src/styles/Order/OrderContainer/StyledRecipientsModalContainer.tsx
+++ src/styles/Order/OrderContainer/StyledRecipientsModalContainer.tsx
@@ -2,7 +2,8 @@ import styled from '@emotion/styled';
 
 export const StyledRecipientsModalContainer = styled.div`
   width: 100%;
-  margin-top: 10px;
+  margin: 20px 0px 20px 0px;
+  padding: 6px 0px;
   display: flex;
   flex-direction: column;
   justify-content: center;
