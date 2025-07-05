cda21404eca41600d614defad15d048224003712
thgus0 <sohyun10521052@gmail.com>
2025-07-06 00:14:21 +0900

    feat: 인증 라우팅 보호를 위한 PrivateRoute 적용

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,22 @@ import { Routes, Route } from 'react-router-dom'
 import { LoginPage } from '@/pages/LoginPage'
 import { NotFoundPage } from '@/pages/NotFoundPage'
 import { Home } from '@/pages/Home'
+import { MyPage } from '@/pages/MyPage'
+import { PrivateRoute } from '@/routes/PrivateRoute'
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/login" element={<LoginPage />} />
+      <Route
+        path="/my"
+        element={
+          <PrivateRoute>
+            <MyPage />
+          </PrivateRoute>
+        }
+      />
       <Route path="*" element={<NotFoundPage />} />
     </Routes>
   )
