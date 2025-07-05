f9f3dd5bc00217d121d930a9671d87c6a6e7d446
ch <chlgur13m@naver.com>
2025-07-05 17:22:47 +0900

    chore(order): style 분리

--- src/components/Order.tsx
+++ src/components/Order.tsx
@@ -6,6 +6,26 @@ import { css } from "@emotion/react";
 import { GiftCardThumb } from "@/components/GiftCardThumb";
 import { useParams } from "react-router-dom";
 import { giftData } from "@/data/giftData";
+import {
+  ThumbNailStyle,
+  ThumbNailContainerStyle,
+  CardWrapperStyle,
+  WrapperStyle,
+  MessageStyle,
+  FormSectionWrapperStyle,
+  ReceiverFormStyle,
+  InputRowStyle,
+  TextStyle,
+  TinyTextStyle,
+  productWrapper,
+  productImage,
+  productInfo,
+  productName,
+  productBrand,
+  productPrice,
+  totalPriceBoxStyle,
+  fixedBottomStyle,
+} from "@/components/Order.style";
 
 const Order: React.FC = () => {
   const theme = useTheme();
@@ -20,7 +40,9 @@ const Order: React.FC = () => {
   useEffect(() => {
     const price = Number(selectedGift?.price.sellingPrice || 0);
     setTotalPrice(quantity * price);
-  }, [quantity]);
+  }, [selectedGift?.price.sellingPrice, quantity]);
+
+  // const nameValidator = () => {};
 
   return (
     <div css={WrapperStyle(theme)}>
@@ -98,164 +120,12 @@ const Order: React.FC = () => {
         </div>
       </div>
       <div css={fixedBottomStyle(theme)}>
-        <div css={totalPriceBoxStyle}>{totalPrice}주문하기</div>
+        <div onClick={() => {}} css={totalPriceBoxStyle}>
+          {totalPrice}주문하기
+        </div>
       </div>
     </div>
   );
 };
 
-const fixedBottomStyle = (theme: Theme) => css`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  width: 100%;
-  position: fixed;
-  max-width: 688px;
-  bottom: 0;
-  padding-bottom: 100px;
-  background-color: ${theme.colors.yellow.yellow500};
-  padding: ${theme.spacing.spacing11};
-`;
-
 export default Order;
-
-const ThumbNailStyle = (
-  theme: Theme,
-  cardId: number,
-  selectedId: number | undefined
-) => css`
-  flex-shrink: 0;
-  padding: ${theme.spacing.spacing0};
-  border: ${selectedId === cardId ? "3px solid black" : "none"};
-  border-radius: 4px;
-  flex-shrink: 0;
-`;
-
-const ThumbNailContainerStyle = (theme: Theme) => css`
-  display: flex;
-  overflow-x: auto;
-  white-space: nowrap;
-  width: 100%;
-  gap: ${theme.spacing.spacing5};
-`;
-
-const CardWrapperStyle = (theme: Theme) => css`
-  width: 100%;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-
-  gap: ${theme.spacing.spacing10};
-`;
-
-const WrapperStyle = (theme: Theme) => css`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  width :100%
-  height : 80%;
-  margin-bottom : 100px;
-  gap: ${theme.spacing.spacing10};
-`;
-
-const MessageStyle = (theme: Theme) => css`
-  display: flex;
-  flex-direction: column;
-  padding: ${theme.spacing.spacing8};
-  width: 100%;
-`;
-
-const FormSectionWrapperStyle = (theme: Theme) => css`
-  width: 100%;
-  padding: ${theme.spacing.spacing8};
-  border-radius: 8px;
-  display: flex;
-  flex-direction: column;
-  gap: ${theme.spacing.spacing3};
-`;
-
-const ReceiverFormStyle = (theme: Theme) => css`
-  display: flex;
-  flex-direction: column;
-  gap: ${theme.spacing.spacing3};
-`;
-
-const InputRowStyle = (theme: Theme) => css`
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  gap: ${theme.spacing.spacing10};
-
-  span {
-    width: 70px;
-  }
-  input {
-    flex: 1;
-    padding: ${theme.spacing.spacing8};
-    border: 1px solid;
-    border-radius: 8px;
-    height: 30px;
-  }
-`;
-
-const TextStyle = (theme: Theme) => css`
-  font-size: ${theme.typography.subtitle1Bold.size};
-  font-weight: ${theme.typography.subtitle1Bold.weight};
-  line-height: ${theme.typography.subtitle1Bold.lineHeight};
-`;
-const TinyTextStyle = () => css`
-  font-size: 12px;
-  color: #888888;
-  margin-top: 4px;
-`;
-
-const productWrapper = (theme: Theme) => css`
-  display: flex;
-  align-items: center;
-  gap: 16px;
-  padding: ${theme.spacing.spacing4};
-  border: 1px solid ${theme.colors.semantic.border.default};
-  border-radius: 8px;
-  width: 100%;
-`;
-
-const productImage = (theme: Theme) => css`
-  width: 100px;
-  height: 100px;
-  object-fit: contain;
-  border-radius: 8px;
-  border: 1px solid ${theme.colors.semantic.border.default};
-`;
-
-const productInfo = (theme: Theme) => css`
-  display: flex;
-  flex-direction: column;
-  gap: ${theme.spacing.spacing2};
-`;
-
-const productName = (theme: Theme) => css`
-  font-weight: ${theme.typography.subtitle1Bold.weight};
-  font-size: ${theme.typography.subtitle1Bold.size};
-  color: ${theme.colors.semantic.text.default};
-`;
-
-const productBrand = (theme: Theme) => css`
-  font-size: ${theme.typography.body2Regular.size};
-  color: ${theme.colors.semantic.text.sub};
-`;
-
-const productPrice = (theme: Theme) => css`
-  font-size: ${theme.typography.body1Regular.size};
-  color: ${theme.colors.semantic.text.default};
-  strong {
-    font-weight: ${theme.typography.subtitle1Bold.weight};
-  }
-`;
-
-const totalPriceBoxStyle = (theme: Theme) => css`
-  font-size: ${theme.typography.title2Bold.size};
-  color: ${theme.colors.semantic.text.default};
-  font-weight: ${theme.typography.subtitle1Bold.weight};
-`;
