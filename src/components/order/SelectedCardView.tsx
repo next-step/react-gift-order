c9683f96e0fb58d4b346b59042003308ef19f34f
kimgho <kgh67811@knu.ac.kr>
2025-07-05 14:04:58 +0900

    feat(Order): 주문 관련 컴포넌트 구현

---
+++ src/components/order/SelectedCardView.tsx
@@ -0,0 +1,81 @@
+import { cardTemplate } from "@/__mock__";
+import { ErrorMessage } from "@/components/common";
+import { useOrder } from "@/hooks/order/useOrder";
+import styled from "@emotion/styled";
+import { useEffect } from "react";
+
+const SelectedCardContainer = styled.div(({ theme }) => ({
+  display: "flex",
+  width: "100%",
+  flexDirection: "column",
+  alignItems: "center",
+  justifyContent: "center",
+  padding: theme.spacing4,
+}));
+
+const SelectedCardImage = styled.img(({ theme }) => ({
+  width: "360px",
+  height: "240px",
+  objectFit: "cover",
+  borderRadius: "8px",
+  boxShadow: "0px 8px 2px rgba(0, 0, 0, 0.1)",
+  marginBottom: theme.spacing8,
+}));
+
+const MessageInput = styled.textarea<{ hasError?: boolean }>(
+  ({ theme, hasError }) => ({
+    display: "flex",
+    width: "100%",
+    borderRadius: "8px",
+    border: `1px solid ${
+      hasError ? theme.color.red[700] : theme.color.gray[500]
+    }`,
+    color: theme.color.gray[900],
+    padding: `${theme.spacing2} ${theme.spacing3}`,
+    fontSize: theme.typography.body2Regular.fontSize,
+    fontWeight: theme.typography.body2Regular.fontWeight,
+    lineHeight: theme.typography.body2Regular.lineHeight,
+    outline: "none",
+    transition: "all 0.2s ease-in-out",
+
+    "&:focus": {
+      border: `1px solid ${
+        hasError ? theme.color.red[500] : theme.color.gray[700]
+      }`,
+      outline: "none",
+    },
+  }),
+);
+
+export const SelectedCardView = () => {
+  const { order, setOrder, register } = useOrder();
+  const messageField = register("message");
+
+  useEffect(() => {
+    if (!order.cardTemplate) {
+      setOrder(prev => ({
+        ...prev,
+        cardTemplate: cardTemplate[0],
+        message: cardTemplate[0].defaultTextMessage,
+      }));
+    }
+  }, [order.cardTemplate, setOrder]);
+
+  return (
+    <SelectedCardContainer>
+      <SelectedCardImage
+        id={String(order.cardTemplate?.id)}
+        src={order.cardTemplate?.imageUrl}
+        alt="선택된 카드 이미지"
+      />
+      <MessageInput
+        placeholder={order.cardTemplate?.defaultTextMessage}
+        value={messageField.value}
+        onChange={messageField.onChange}
+        onBlur={messageField.onBlur}
+        hasError={messageField.hasError}
+      />
+      {messageField.error && <ErrorMessage>{messageField.error}</ErrorMessage>}
+    </SelectedCardContainer>
+  );
+};
---
+++ src/components/order/index.tsx
@@ -0,0 +1,7 @@
+export { CardSelectorBar } from "@/components/order/CardSelectorBar";
+export { OrderButton } from "@/components/order/OrderButton";
+export { OrderLayout } from "@/components/order/OrderLayout";
+export { OrderProductInfoSection } from "@/components/order/OrderProductInfoSection";
+export { ReceiverInfoSection } from "@/components/order/ReceiverInfoSection";
+export { SelectedCardView } from "@/components/order/SelectedCardView";
+export { SenderInfoSection } from "@/components/order/SenderInfoSection";
