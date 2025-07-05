a5cfb0e1d7d8f96a868dd1b0c6e04abc8fc5a072
ch <chlgur13m@naver.com>
2025-07-05 13:17:41 +0900

    feat(order): 입력폼 추가

--- src/components/Order.tsx
+++ src/components/Order.tsx
@@ -46,7 +46,37 @@ const Order: React.FC = () => {
           }
         ></img>
       </div>
-      <textarea defaultValue="축하해요." css={TextStyle()}></textarea>
+      <div css={MessageStyle(theme)}>
+        <textarea defaultValue="축하해요."></textarea>
+      </div>
+
+      <div css={FormSectionWrapperStyle(theme)}>
+        <p css={TextStyle(theme)}>보내는 사람</p>
+        <div css={InputRowStyle(theme)}>
+          <input placeholder="이름을 입력하세요."></input>
+        </div>
+        <p css={TinyTextStyle}>
+          * 실제 선물 발송 시 발신자이름으로 반영되는 정보입니다.
+        </p>
+      </div>
+
+      <div css={FormSectionWrapperStyle(theme)}>
+        <p css={TextStyle(theme)}>받는 사람</p>
+        <div css={ReceiverFormStyle(theme)}>
+          <div css={InputRowStyle(theme)}>
+            <span>이름</span>
+            <input placeholder="이름을 입력하세요."></input>
+          </div>
+          <div css={InputRowStyle(theme)}>
+            <span>전화번호</span>
+            <input placeholder="전화번호를 입력하세요."></input>
+          </div>
+          <div css={InputRowStyle(theme)}>
+            <span>수량</span>
+            <input defaultValue={1}></input>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
@@ -87,8 +117,56 @@ const WrapperStyle = (theme: Theme) => css`
   gap: ${theme.spacing.spacing10};
 `;
 
-const TextStyle = () => css`
+const MessageStyle = (theme: Theme) => css`
+  display: flex;
+  flex-direction: column;
+  padding: ${theme.spacing.spacing8};
   width: 100%;
-  height: 60px;
-  border-radius: 4px;
+`;
+
+const FormSectionWrapperStyle = (theme: Theme) => css`
+  width: 100%;
+  padding: ${theme.spacing.spacing8}; // 안쪽 여백
+  background-color:${theme.colors.gray.gray200}
+  border-radius: 8px;
+  display: flex;
+  flex-direction: column;
+  gap: ${theme.spacing.spacing3}
+`;
+
+const ReceiverFormStyle = (theme: Theme) => css`
+  display: flex;
+  flex-direction: column;
+  gap: ${theme.spacing.spacing3};
+`;
+
+const InputRowStyle = (theme: Theme) => css`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  gap: ${theme.spacing.spacing10};
+  
+
+  span {
+    width: 70px;
+  }
+  input {
+    flex: 1;
+    padding: ${theme.spacing.spacing8}
+    border: 1px solid
+    border-radius: 8px;
+    height : 50px;
+  }
+`;
+
+const TextStyle = (theme: Theme) => css`
+  font-size: ${theme.typography.subtitle1Bold.size};
+  font-weight: ${theme.typography.subtitle1Bold.weight};
+  line-height: ${theme.typography.subtitle1Bold.lineHeight};
+`;
+
+const TinyTextStyle = () => css`
+  font-size: 12px;
+  color: #888888;
+  margin-top: 4px;
 `;
