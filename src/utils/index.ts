82cfb0a66747d37a16c618119520b04cbc3d7924
kimgho <kgh67811@knu.ac.kr>
2025-07-05 13:58:56 +0900

    feat(validation): 유효성 검증을 위한 validation.ts 변경

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,2 @@
 export * from "@/utils/storage";
-export * from "@/utils/validation";
 export * from "@/utils/login-validator";
--- src/utils/login-validator.ts
+++ src/utils/login-validator.ts
@@ -1,30 +1,28 @@
 import { EMAIL_REGEX, LOGIN_ERROR_MESSAGE } from "@/constants";
-import { validateField } from "@/utils/validation";
+import type { ValidationRulesMap } from "@/utils/type";
+import type { FormData } from "@/utils/type";
 
 export const isValidEmail = (email: string): boolean => EMAIL_REGEX.test(email);
 
-export const loginValidators = {
-  id: (value: string) =>
-    validateField(value, [
-      {
-        condition: val => !val.trim(),
-        message: LOGIN_ERROR_MESSAGE.ID.REQUIRED,
-      },
-      {
-        condition: val => !isValidEmail(val),
-        message: LOGIN_ERROR_MESSAGE.ID.INVALID_FORMAT,
-      },
-    ]),
-
-  password: (value: string) =>
-    validateField(value, [
-      {
-        condition: val => !val.trim(),
-        message: LOGIN_ERROR_MESSAGE.PASSWORD.REQUIRED,
-      },
-      {
-        condition: val => val.length < 8,
-        message: LOGIN_ERROR_MESSAGE.PASSWORD.MIN_LENGTH,
-      },
-    ]),
+export const loginValidationRules: ValidationRulesMap<FormData> = {
+  id: [
+    {
+      condition: (value: string) => !value.trim(),
+      message: LOGIN_ERROR_MESSAGE.ID.REQUIRED,
+    },
+    {
+      condition: (value: string) => !isValidEmail(value),
+      message: LOGIN_ERROR_MESSAGE.ID.INVALID_FORMAT,
+    },
+  ],
+  password: [
+    {
+      condition: (value: string) => !value.trim(),
+      message: LOGIN_ERROR_MESSAGE.PASSWORD.REQUIRED,
+    },
+    {
+      condition: (value: string) => value.length < 8,
+      message: LOGIN_ERROR_MESSAGE.PASSWORD.MIN_LENGTH,
+    },
+  ],
 };
