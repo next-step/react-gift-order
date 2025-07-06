bce580e8a8952cb3b3467b5df29ab9eee15ff406
leehuiseok <leehuiseok020412@gmail.com>
2025-07-07 02:21:09 +0900

    feat: 로그인 정보 Context API

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import { Router } from '@/shared/Router';
+import { AuthProvider } from '@/contexts/AuthContext';
 
 function App() {
-  return <Router />;
+  return (
+    <AuthProvider>
+      <Router />
+    </AuthProvider>
+  );
 }
 
 export default App;
