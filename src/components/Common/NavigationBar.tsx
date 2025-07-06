2d9611e55315d5340ce02803c50cdac97d6f3739
hojunlee <leehojun.full@gmail.com>
2025-07-06 21:13:25 +0900

    feat : feedback appy - 외부에서 url로 접근한 경우 선물하기가 아닌 원래 있던 페이지로 이동하는 문제 해결

--- src/components/Common/NavigationBar.tsx
+++ src/components/Common/NavigationBar.tsx
@@ -6,7 +6,7 @@ import { StyledNavTextDiv } from '@/styles/Common/StyledNavTextDiv';
 import { URLS } from '@assets/urls';
 import { Spacer } from '@styles/Spacer';
 import { useEffect, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 const NavigationBar = () => {
   const navigate = useNavigate();
@@ -14,9 +14,28 @@ const NavigationBar = () => {
   const LOGIN_URL = URLS.login;
   const MyPage = URLS.mypage;
   const isLogined = sessionStorage.getItem('username');
+  const location = useLocation();
 
   const handelBack = () => {
-    navigate(-1);
+    const referrer = document.referrer; // 이전 페이지의 URL
+    const currentOrigin = window.location.origin;
+
+    if (referrer) {
+      // referrer가 존재한다면, 해당 URL의 origin을 파싱합니다.
+      const referrerUrl = new URL(referrer);
+      const referrerOrigin = referrerUrl.origin;
+
+      if (referrerOrigin !== currentOrigin) {
+        // 이전 페이지의 origin이 현재 우리 사이트의 origin과 다르면
+        navigate(URLS.home); // 또는 navigate('/')
+      } else {
+        // 이전 페이지의 origin이 우리 사이트와 같으면
+        navigate(-1);
+      }
+    } else {
+      console.log('referrer 없음. 홈으로 이동합니다.');
+      navigate(URLS.home); // 또는 navigate('/')
+    }
   };
   const handelLogin = () => {
     if (isLogined) {
