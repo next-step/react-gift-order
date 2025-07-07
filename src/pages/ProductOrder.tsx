7480946f627d3766f115678d250e6ebe223bba13
kanghaeun <xgkg0330@jnu.ac.kr>
2025-07-07 17:36:27 +0900

    feat: 선물 카드 선택 옵션 추가

--- src/pages/ProductOrder.tsx
+++ src/pages/ProductOrder.tsx
@@ -1,7 +1,32 @@
-import React from 'react';
+import NavigationBar from '@/common/NavigationBar';
+import GiftCardSelector from '@/components/ProductOrder/GiftCardSelector';
+import styled from '@emotion/styled';
 
 const ProductOrder = () => {
-  return <div>ProductOrder</div>;
+  return (
+    <Layout>
+      <Content>
+        <NavigationBar />
+        <GiftCardSelector />
+      </Content>
+    </Layout>
+  );
 };
 
 export default ProductOrder;
+
+const Layout = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding-top: 44px;
+  justify-content: flex-start;
+  align-items: center;
+  width: 100%;
+  height: 100vh;
+  gap: ${({ theme }) => theme.spacing.spacing2};
+`;
+
+const Content = styled.div`
+  width: 100%;
+  max-width: 720px;
+`;
