0d88b1d317c155af3e6085d6a4251e2d1346325b
daaoooy <y020d226y@daum.net>
2025-07-07 17:38:49 +0900

    feat(login) : 로그인 정보 관리 및 유지 관련 기능 추가
    - 닉네임 추가
    - 상품 클릭 시 로그인화면으로 이동했을 땐, 로그인 완료 후 상품 클릭 후  페이지로 이동
    - Context API 와 세션 스토리지를 통한 로그인 정보 유지 및 사용, 관리
      기능 추가

--- src/features/Login/components/LoginForm.tsx
+++ src/features/Login/components/LoginForm.tsx
@@ -5,12 +5,17 @@ import {
   InputForm,
   ErrorMessage,
 } from './LoginForm.styles'
-import { useNavigate, useLocation } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 import KaKaoTitleIcon from '@/assets/icons/kakao-title.svg?react'
 import MyButton from '@/component/Button/Button'
 import { useLoginForm } from '../hooks/useLoginForm'
+import { useUserContext } from '@/contexts/UserContext'
 
-const LoginForm: React.FC = () => {
+interface LoginFormProps {
+  redirectPath: string
+}
+
+const LoginForm: React.FC<LoginFormProps> = ({ redirectPath }) => {
   const {
     email,
     setEmail,
@@ -21,27 +26,22 @@ const LoginForm: React.FC = () => {
     checkEmail,
     checkPassword,
     loginOK,
-    emailTouched,
     setEmailTouched,
-    passwordTouched,
     setPasswordTouched,
   } = useLoginForm()
 
+  const { login } = useUserContext()
   const navigate = useNavigate()
-  const location = useLocation()
-  const hasPreviousPage = location.key !== 'default'
 
   const handleLoginClick = () => {
     const emailValid = checkEmail()
     const passwordValid = checkPassword()
     if (!emailValid || !passwordValid) return
-    console.log('로그인 요청:', { email, password })
 
-    if (hasPreviousPage) {
-      navigate(-1)
-    } else {
-      navigate('/')
-    }
+    const nickname = email.split('@')[0]
+    login({ email, nickname })
+
+    navigate(redirectPath, { replace: true })
   }
 
   return (
@@ -55,9 +55,7 @@ const LoginForm: React.FC = () => {
           placeholder="이메일"
           type="email"
           value={email}
-          onChange={(e) => {
-            setEmail(e.target.value)
-          }}
+          onChange={(e) => setEmail(e.target.value)}
           onBlur={() => setEmailTouched(true)}
           isError={!!emailError}
         />
@@ -79,8 +77,10 @@ const LoginForm: React.FC = () => {
         <MyButton
           onClick={handleLoginClick}
           variant="primary"
+          size="large"
           disabled={!loginOK()}
-          fullWidth={true}
+          fullWidth
+          style={{ marginTop: '32px' }}
         >
           로그인
         </MyButton>
--- src/features/Login/pages/LoginPage.tsx
+++ src/features/Login/pages/LoginPage.tsx
@@ -1,7 +1,12 @@
+import { useLocation } from 'react-router-dom'
 import LoginForm from '@/features/Login/components/LoginForm'
 
 const LoginPage = () => {
-  return <LoginForm />
+  const location = useLocation()
+  const from = location.state?.from || { pathname: '/' }
+  const redirectPath = from.pathname + (from.search || '')
+
+  return <LoginForm redirectPath={redirectPath} />
 }
 
 export default LoginPage
