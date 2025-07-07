7fa13b170ce9b0c4c5723cf28b7b061b749c9253
Changmok LEE <cmlee5075@gmail.com>
2025-07-07 16:12:20 +0900

    feat: 조건부 라우팅 구현 (로그인 상태에 따른)

--- src/routes/Routers.tsx
+++ src/routes/Routers.tsx
@@ -1,14 +1,17 @@
 import GiftsPage from '@/page/GiftPage';
 import LoginPage from '@/page/LoginPage';
+import MyPage from '@/page/MyPage';
 import NotFound from '@/page/NotFound';
 import { Route, Routes } from 'react-router-dom';
 import { ROUTES } from './routes';
+import RequireAuth from './RequireAuth';
 
 const AppRouter = () => {
   return (
     <Routes>
       <Route path={ROUTES.HOME} element={<GiftsPage />} />
       <Route path={ROUTES.LOGIN} element={<LoginPage />} />
+      <Route path={ROUTES.MY} element={<RequireAuth><MyPage /></RequireAuth>} />
       <Route path={ROUTES.NOTFOUND} element={<NotFound />} />
     </Routes>
   );
--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,5 +1,6 @@
 export const ROUTES = {
   HOME: '/',
   LOGIN: '/login',
+  MY: '/my',
   NOTFOUND: '*',
 } as const;
