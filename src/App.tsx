f02ad1e0a37b4d133e1a785381223d2e22abd37f
thighnurger <lewis177777gmail.com>
2025-07-07 15:10:26 +0900

    feat: 보호된 라우트 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,27 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import LoginPage from '@/pages/login'
-import Home from '@/pages/home'
+import HomePage from '@/pages/HomePage'
 import NotFound from '@/pages/NotFound'
+import MyPage from '@/pages/MyPage'
+import { AuthProvider } from '@/contexts/AuthContext'
+import ProtectedRoute from '@/components/ProtectedRoute'
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/" element={<Home />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </BrowserRouter>
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/" element={<HomePage />} />
+          <Route path="/my" element={
+            <ProtectedRoute>
+              <MyPage />
+            </ProtectedRoute>
+          } />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
   )
 }
 
