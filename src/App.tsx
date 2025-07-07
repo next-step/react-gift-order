5c10d13d5744ba3dcb783abd78e34543695554b4
HanseKim <kimbird2002@naver.com>
2025-07-07 10:58:30 +0900

    feat: 로컬스토리지, Context API로 로그인 정보 저장

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from 'react';
 import { ThemeProvider } from '@emotion/react';
 import { theme } from '@/styles/ResetStyles';
 import { AppWrapper } from '@/styles/App.styles';
@@ -7,37 +8,53 @@ import WithHeaderLayout from '@/Layout/WithHeaderLayout';
 import ResetStyles from '@/styles/ResetStyles';
 import MainLayout from '@/Layout/MainLayout';
 import Login from '@/pages/Login';
+import Mypage from './pages/Mypage';
 import NotFound from '@/NotFound';
+import { LoginInfoContext } from '@/contexts/LoginInfoContext';
 
 function App() {
+  const [loginInfo, setLoginInfo] = useState<string>('');
   const navigate = useNavigate();
   const location = useLocation();
+
   function handleBackClick() {
     if (location.pathname !== '/') navigate(-1);
   }
+
   function handleLoginClick() {
-    navigate('/login');
+    if (loginInfo == '') navigate('/login');
+    else {
+      navigate('/my');
+    }
   }
+
+  useEffect(() => {
+    setLoginInfo(localStorage.getItem('id') || '');
+  }, []);
+
   return (
-    <ThemeProvider theme={theme}>
-      <AppWrapper>
-        <ResetStyles />
-        <Routes>
-          <Route
-            element={
-              <WithHeaderLayout
-                handleBackClick={handleBackClick}
-                handleLoginClick={handleLoginClick}
-              />
-            }
-          >
-            <Route path="/" element={<MainLayout />} />
-            <Route path="/login" element={<Login onLogin={handleBackClick} />} />
-          </Route>
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </AppWrapper>
-    </ThemeProvider>
+    <LoginInfoContext.Provider value={{ loginInfo, setLoginInfo }}>
+      <ThemeProvider theme={theme}>
+        <AppWrapper>
+          <ResetStyles />
+          <Routes>
+            <Route
+              element={
+                <WithHeaderLayout
+                  handleBackClick={handleBackClick}
+                  handleLoginClick={handleLoginClick}
+                />
+              }
+            >
+              <Route path="/" element={<MainLayout />} />
+              <Route path="/login" element={<Login onLogin={handleBackClick} />} />
+              <Route path="/my" element={<Mypage onLogin={handleBackClick} />} />
+            </Route>
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </AppWrapper>
+      </ThemeProvider>
+    </LoginInfoContext.Provider>
   );
 }
 
