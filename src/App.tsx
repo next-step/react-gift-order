caf6a812d4edf98681a885bfa4432430db4fd10c
cktjgus <cktjgus04@naver.com>
2025-07-07 23:24:22 +0900

    path 상수화 및 createBrowserRouter 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,41 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import { ThemeProvider, Global } from '@emotion/react'
-import { resetStyle } from '@/styles/reset'
-import theme from '@/styles/theme'
-import HomePage from '@/pages/HomePage'
-import LoginPage from '@/pages/Login/LoginPage'
-import NotFoundPage from '@/pages/NotfoundPage'
-import { MainLayout } from './components/MainLayout'
-import MyPage from '@/pages/MyPage'
-import OrderPage from '@/pages/OrderPage'
+import { ThemeProvider, Global } from '@emotion/react';
+import { RouterProvider, createBrowserRouter, Outlet } from 'react-router-dom';
+import { resetStyle } from '@/styles/reset';
+import theme from '@/styles/theme';
+import { PATH } from '@/paths';
+import { MainLayout } from '@/components/MainLayout';
+import HomePage from '@/pages/HomePage';
+import LoginPage from '@/pages/Login/LoginPage';
+import MyPage from '@/pages/MyPage';
+import NotFoundPage from '@/pages/NotfoundPage';
+import OrderPage from '@/pages/OrderPage';
+
+const LayoutWrapper = () => (
+  <MainLayout>
+    <Outlet />
+  </MainLayout>
+);
+
+const router = createBrowserRouter([
+  {
+    element: <LayoutWrapper />,
+    children: [
+      { path: PATH.HOME, element: <HomePage /> },
+      { path: PATH.LOGIN, element: <LoginPage /> },
+      { path: PATH.MY, element: <MyPage /> },
+      { path: PATH.ORDER, element: <OrderPage /> },
+      { path: PATH.NOTFOUND, element: <NotFoundPage /> },
+    ],
+  },
+]);
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <Global styles={resetStyle} />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<MainLayout><HomePage /></MainLayout>} />
-          <Route path="/login" element={<MainLayout><LoginPage /></MainLayout>} />
-          <Route path="*" element={<MainLayout><NotFoundPage /></MainLayout>} />
-          <Route path="/my" element={<MainLayout><MyPage /></MainLayout>} />
-          <Route path="/order/:itemId" element={<MainLayout><OrderPage /></MainLayout>} />
-        </Routes>
-      </BrowserRouter>
+      <RouterProvider router={router} />
     </ThemeProvider>
-  )
+  );
 }
 
-export default App
+export default App;
