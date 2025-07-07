3431d4b1067df8cd9fa07c98e127b4a640728264
thighnurger <lewis177777gmail.com>
2025-07-07 17:05:39 +0900

    feat: PageWrapper 컴포넌트로 레이아웃 래핑

--- src/pages/MyPage.tsx
+++ src/pages/MyPage.tsx
@@ -1,6 +1,5 @@
 import Header from '@/components/Header'
 import GlobalStyle from '@/styles/GlobalStyle'
-import PageWrapper from '@/components/PageWrapper'
 import { useAuth } from '@/contexts/AuthContext'
 import { useNavigate } from 'react-router-dom'
 
@@ -12,41 +11,39 @@ function MyPage() {
   const email = user?.email || ''
 
   return (
-    <>
+    <div style={{ maxWidth: 720, margin: '0 auto' , alignItems: 'center'}}>
       <GlobalStyle />
       <Header />
-      <PageWrapper>
-        <div>
-          <div>
-            <div>마이 페이지</div>
-            <div>
-              {nickname}님 안녕하세요!<br />
-              이메일 주소는 {email}입니다.
-            </div>
-            <button 
-              onClick={() => {
-                logout();
-                navigate('/login');
-              }}
-              style={{
-                padding: '8px 20px',
-                background: '#f2f2f2',
-                color: '#444',
-                border: 'none',
-                borderRadius: 6,
-                fontWeight: 500,
-                fontSize: 15,
-                cursor: 'pointer',
-                marginTop: 8
-              }}
-            >
-              로그아웃
-            </button>
+      <div >
+        <div >
+          <div>마이 페이지</div>
+          <div >
+            {nickname}님 안녕하세요!<br />
+            이메일 주소는 {email}입니다.
           </div>
-          <div />
+          <button 
+            onClick={() => {
+              logout();
+              navigate('/login');
+            }}
+            style={{
+              padding: '8px 20px',
+              background: '#f2f2f2',
+              color: '#444',
+              border: 'none',
+              borderRadius: 6,
+              fontWeight: 500,
+              fontSize: 15,
+              cursor: 'pointer',
+              marginTop: 8
+            }}
+          >
+            로그아웃
+          </button>
         </div>
-      </PageWrapper>
-    </>
+        <div />
+      </div>
+    </div>
   )
 }
 
