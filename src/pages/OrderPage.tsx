8132bb3beff81336539ce8d01f26a2f58aca6d22
Injun Jeong <jijun897@naver.com>
2025-07-07 14:33:11 +0900

    fix: 로그인 상태에도 새로고침시 로그인 페이지로 이동하는 문제 수정

--- src/pages/OrderPage.tsx
+++ src/pages/OrderPage.tsx
@@ -1,7 +1,6 @@
 import TheHeader from "@/components/layout/TheHeader";
 import { useParams, useLocation, useNavigate } from "react-router";
 import { useEffect } from "react";
-import { useUserInfo } from "@/contexts/UserInfoContext";
 import { ROUTE_PATH } from "@/routes/paths";
 import styled from "@emotion/styled";
 import CardSection from "@/components/order/CardSection";
@@ -9,17 +8,17 @@ import SendSection from "@/components/order/SendSection";
 import ReceiverSection from "@/components/order/ReceiverSection";
 
 const OrderPage = () => {
-  const user = useUserInfo();
   const location = useLocation();
   const navigate = useNavigate();
 
   useEffect(() => {
-    if (!user?.email) {
+    const sessionUserInfo = sessionStorage.getItem("kakaotech/userInfo");
+    if (!sessionUserInfo) {
       navigate(`${ROUTE_PATH.LOGIN}?redirect=${location.pathname}`, {
         replace: true,
       });
     }
-  }, [user, location.pathname, navigate]);
+  }, [location.pathname, navigate]);
 
   const { id } = useParams<{ id: string }>();
 
@@ -30,9 +29,7 @@ const OrderPage = () => {
         <CardSection />
         <SendSection />
         <ReceiverSection />
-        <h1>주문 페이지</h1>
         <p>선택한 선물의 ID: {id}</p>
-        <p>로그인한 사용자: {user?.email || "게스트"}</p>
       </Main>
     </>
   );
