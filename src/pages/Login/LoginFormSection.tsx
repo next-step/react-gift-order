81b61309291bd9cb6cec523028ca949ca4f60448
eunchae-04 <rhdmsco1163@naver.com>
2025-07-07 14:36:25 +0900

    refactor: handleLoginAndRedirect 함수명을 loginWithRedirect로 변경

--- src/pages/Login/LoginFormSection.tsx
+++ src/pages/Login/LoginFormSection.tsx
@@ -24,7 +24,7 @@ const LoginFormSection = () => {
     goToLogin,
   } = useLoginForm();
 
-  const handleLoginAndRedirect = () => {
+  const loginWithRedirect = () => {
     goToLogin();
     navigate(redirectPath, { replace: true });
   };
@@ -60,7 +60,7 @@ const LoginFormSection = () => {
       <button
         css={loginButtonStyle(theme)}
         disabled={!isValid}
-        onClick={handleLoginAndRedirect}
+        onClick={loginWithRedirect}
       >
         로그인
       </button>
