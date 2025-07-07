402b5321bd8bfee418acc8abbf9a52de8a976cb2
daaoooy <y020d226y@daum.net>
2025-07-07 17:30:31 +0900

    feat(route) : 로그인 여부에 따른 라우트 기능 추가
    - order 페이지와 my 페이지로 이동하는 기능 추가
    - PrivateRoute 를 통해 로그인 여부에 따라 이동하는 기능 추가

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,16 @@ import { BrowserRouter } from 'react-router-dom'
 import { Global } from '@emotion/react'
 import { globalStyle } from '@/styles/GlobalStyle'
 import AppRouter from '@/routes/Router'
+import { UserProvider } from '@/contexts/UserContext'
 
 const App = () => {
   return (
     <>
       <Global styles={globalStyle} />
       <BrowserRouter>
-        <AppRouter />
+        <UserProvider>
+          <AppRouter />
+        </UserProvider>
       </BrowserRouter>
     </>
   )
--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -4,6 +4,9 @@ import GiftPage from '@/features/Gift/pages/GiftPage'
 import LoginPage from '@/features/Login/pages/LoginPage'
 import NotFoundPage from '@/features/NotFound/pages/NotFoundPage'
 import NavLayout from '@/component/Layout/NavLayout'
+import MyPage from '@/features/My/pages/MyPage'
+import OrderPage from '@/features/Order/pages/OrderPage'
+import PrivateRoute from '@/routes/PrivateRoute'
 
 const AppRouter = () => {
   return (
@@ -12,6 +15,22 @@ const AppRouter = () => {
         <Route path={ROUTE_PATH.GIFT} element={<GiftPage />} />
         <Route path={ROUTE_PATH.LOGIN} element={<LoginPage />} />
         <Route path={ROUTE_PATH.NOT_FOUND} element={<NotFoundPage />} />
+        <Route
+          path={ROUTE_PATH.MY}
+          element={
+            <PrivateRoute>
+              <MyPage />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path={ROUTE_PATH.ORDER}
+          element={
+            <PrivateRoute>
+              <OrderPage />
+            </PrivateRoute>
+          }
+        />
       </Route>
     </Routes>
   )
@@ -20,6 +39,8 @@ const AppRouter = () => {
 export const ROUTE_PATH = {
   GIFT: '/',
   LOGIN: '/login',
+  MY: '/my',
+  ORDER: '/order',
   NOT_FOUND: '*',
 }
 
