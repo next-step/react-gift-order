7c144e5069d32aaa41da4d55cbb3271bbb40bb22
kimgho <kgh67811@knu.ac.kr>
2025-07-07 20:53:14 +0900

    refactor(order-form): Register 타입 분리

--- src/contexts/order/types.ts
+++ src/contexts/order/types.ts
@@ -1,27 +1,20 @@
 import type { Order } from "@/types";
+import type { Register } from "@/types/form-register-type";
 import type { ValidationErrors } from "@/utils/type";
-import type { ChangeEvent, Dispatch, FormEvent, SetStateAction } from "react";
+import type { FormEvent } from "react";
 
 export interface OrderContextType {
   order: Order;
   errors: ValidationErrors<Order>;
   touched: Record<keyof Order, boolean>;
-  register: <K extends keyof Order>(
-    field: K,
-  ) => {
-    name: K;
-    value: Order[K];
-    onChange: (
-      e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement> | Order[K],
-    ) => void;
-    onBlur: () => void;
-    error: string | undefined;
-    hasError: boolean;
-  };
+  register: Register<Order>;
   handleSubmit: (e: FormEvent<HTMLFormElement>) => void;
   resetOrder: () => void;
-  setOrder: Dispatch<SetStateAction<Order>>;
+  setOrder: (
+    newValues: Partial<Order> | ((prev: Order) => Partial<Order>),
+  ) => void;
   isOrderComplete: () => boolean;
   getValidationErrors: () => string[];
   calculateTotalPrice: () => number;
+  validateAllFields: () => boolean;
 }
