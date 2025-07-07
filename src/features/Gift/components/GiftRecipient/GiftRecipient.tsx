3af450efd587fc622b5dd2999305845983223dc3
daaoooy <y020d226y@daum.net>
2025-07-07 17:41:45 +0900

    feat(nickname) : 로그인 여부에 따라 닉네임 뜨는 기능 추가

--- src/features/Gift/components/GiftRecipient/GiftRecipient.tsx
+++ src/features/Gift/components/GiftRecipient/GiftRecipient.tsx
@@ -1,10 +1,17 @@
 import { Container, PlusButton, Text } from './GiftRecipient.styles'
+import { useUserContext } from '@/contexts/UserContext'
 
 const GiftRecipient: React.FC = () => {
+  const { user, isLoggedIn } = useUserContext()
+
   return (
     <Container>
       <PlusButton>＋</PlusButton>
-      <Text>선물할 친구를 선택해 주세요.</Text>
+      {isLoggedIn ? (
+        <Text>{user?.nickname}님! 선물할 친구를 선택해 주세요.</Text>
+      ) : (
+        <Text>선물할 친구를 선택해 주세요.</Text>
+      )}
     </Container>
   )
 }
