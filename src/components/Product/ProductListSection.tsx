c248ac25d117d2d191172c2565ca9b4567c75579
thgus0 <sohyun10521052@gmail.com>
2025-07-07 17:44:13 +0900

    refactor: productMock 분리

--- src/components/Product/ProductListSection.tsx
+++ src/components/Product/ProductListSection.tsx
@@ -1,24 +1,7 @@
 import styled from '@emotion/styled'
 import { useEffect, useState, useMemo } from 'react'
 import { ProductItem } from '@/components/Product/ProductItem'
-
-const productMock = Array(21).fill({
-  id: 123,
-  name: 'BBQ 양념치킨+크림치즈볼+콜라1.25L',
-  imageURL:
-    'https://st.kakaocdn.net/product/gift/product/20231030175450_53e90ee9708f45ffa45b3f7b4bc01c7c.jpg',
-  price: {
-    basicPrice: 29000,
-    discountRate: 0,
-    sellingPrice: 29000,
-  },
-  brandInfo: {
-    id: 2088,
-    name: 'BBQ',
-    imageURL:
-      'https://st.kakaocdn.net/product/gift/gift_brand/20220216170226_38ba26d8eedf450683200d6730757204.png',
-  },
-})
+import { productMock } from '@/components/Product/productMock'
 
 const genderOptions = ['전체', '여성이', '남성이', '청소년이']
 const topicOptions = ['받고 싶어한', '많이 선물한', '위시로 받은']
