da3e1ef26e73593715ec7d56df358fbe67439c9c
thighburger <lewis177777@gmail.com>
2025-07-09 21:26:42 +0900

    refactor:관련된 상태들 하나의 객체로 통합

--- src/pages/OrderPage.tsx
+++ src/pages/OrderPage.tsx
@@ -174,21 +174,34 @@ const OrderPage = () => {
   
   // 가져온 orderCard.ts 데이터 사용
   const [selectedCard, setSelectedCard] = useState<OrderCard>(orderCardsData[0]);
-  const [message, setMessage] = useState(orderCardsData[0].defaultTextMessage);
   
   // 폼 입력 상태 관리
-  const [senderName, setSenderName] = useState('');
-  const [receiverName, setReceiverName] = useState('');
-  const [phoneNumber, setPhoneNumber] = useState('');
-  const [quantity, setQuantity] = useState(1);
-  
+  const [formState, setFormState] = useState({
+    senderName: '',
+    receiverName: '',
+    phoneNumber: '',
+    quantity: 1,
+    message: orderCardsData[0].defaultTextMessage,
+  });
+
   // 오류 메시지 상태
-  const [messageError, setMessageError] = useState('');
-  const [senderNameError, setSenderNameError] = useState('');
-  const [receiverNameError, setReceiverNameError] = useState('');
-  const [phoneNumberError, setPhoneNumberError] = useState('');
-  const [quantityError, setQuantityError] = useState('');
-  
+  const [errorState, setErrorState] = useState({
+    messageError: '',
+    senderNameError: '',
+    receiverNameError: '',
+    phoneNumberError: '',
+    quantityError: '',
+  });
+
+  // 상태 업데이트 핸들러
+  const updateFormState = (field: string, value: string | number) => {
+    setFormState((prevState) => ({ ...prevState, [field]: value }));
+  };
+
+  const updateErrorState = (field: string, value: string) => {
+    setErrorState((prevState) => ({ ...prevState, [field]: value }));
+  };
+
   // 로그인 체크
   useEffect(() => {
     if (!isAuthenticated) {
@@ -198,8 +211,8 @@ const OrderPage = () => {
   
   // 카드 선택 핸들러
   const handleCardSelect = (card: OrderCard) => {
+    setFormState((prevState) => ({ ...prevState, message: card.defaultTextMessage }));
     setSelectedCard(card);
-    setMessage(card.defaultTextMessage);
   };
   
   // 주문하기 핸들러
@@ -207,52 +220,52 @@ const OrderPage = () => {
     let isValid = true;
 
     // 메시지 유효성 검사
-    if (!message.trim()) {
-      setMessageError(`메시지는 반드시 입력되어야해요`);
+    if (!formState.message.trim()) {
+      updateErrorState('messageError', `메시지는 반드시 입력되어야해요`);
       isValid = false;
     } else {
-      setMessageError('');
+      updateErrorState('messageError', '');
     }
 
     // 보내는 사람 유효성 검사
-    if (!senderName.trim()) {
-      setSenderNameError('보내는 사람 이름이 반드시 입력되어야해요');
+    if (!formState.senderName.trim()) {
+      updateErrorState('senderNameError', '보내는 사람 이름이 반드시 입력되어야해요');
       isValid = false;
     } else {
-      setSenderNameError('');
+      updateErrorState('senderNameError', '');
     }
 
     // 받는 사람 유효성 검사
-    if (!receiverName.trim()) {
-      setReceiverNameError(`받는 사람 이름이 반드시 입력되어야해요`);
+    if (!formState.receiverName.trim()) {
+      updateErrorState('receiverNameError', `받는 사람 이름이 반드시 입력되어야해요`);
       isValid = false;
     } else {
-      setReceiverNameError('');
+      updateErrorState('receiverNameError', '');
     }
 
     // 전화번호 유효성 검사 (010으로 시작, 11자리, 숫자)
     const phoneRegex = /^010\d{8}$/;
-    if (!phoneNumber.trim()) {
-      setPhoneNumberError(`받는사람 전화번호가 반드시 입력되어야해요`);
+    if (!formState.phoneNumber.trim()) {
+      updateErrorState('phoneNumberError', `받는사람 전화번호가 반드시 입력되어야해요`);
       isValid = false;
-    } else if (!phoneRegex.test(phoneNumber)) {
-      setPhoneNumberError('전화번호는 010으로 시작하는 11자리 숫자여야 해요.');
+    } else if (!phoneRegex.test(formState.phoneNumber)) {
+      updateErrorState('phoneNumberError', '전화번호는 010으로 시작하는 11자리 숫자여야 해요.');
       isValid = false;
     } else {
-      setPhoneNumberError('');
+      updateErrorState('phoneNumberError', '');
     }
 
     // 수량 유효성 검사
-    if (quantity < 1) {
-      setQuantityError('수량은 1개 이상이어야 해요.');
+    if (formState.quantity < 1) {
+      updateErrorState('quantityError', '수량은 1개 이상이어야 해요.');
       isValid = false;
     } else {
-      setQuantityError('');
+      updateErrorState('quantityError', '');
     }
 
     // 모든 유효성 검사를 통과했을 때만 주문 완료
     if (isValid) {
-      alert(`\n주문 정보:\n- 상품명: ${product.name}\n- 구매수량: ${quantity}개\n- 발신자: ${senderName}\n- 메시지: ${message}\n- 수신자: ${receiverName}\n\n주문이 완료되었습니다!`);
+      alert(`\n주문 정보:\n- 상품명: ${product.name}\n- 구매수량: ${formState.quantity}개\n- 발신자: ${formState.senderName}\n- 메시지: ${formState.message}\n- 수신자: ${formState.receiverName}\n\n주문이 완료되었습니다!`);
       navigate('/');
     }
   };
@@ -293,10 +306,10 @@ const OrderPage = () => {
         <textarea 
           css={messageInput} 
           placeholder="상대에게 보낼 메시지를 입력하세요." 
-          value={message}
-          onChange={(e) => setMessage(e.target.value)}
+          value={formState.message}
+          onChange={(e) => updateFormState('message', e.target.value)}
         />
-        {messageError && <div css={errorMessage}>{messageError}</div>}
+        {errorState.messageError && <div css={errorMessage}>{errorState.messageError}</div>}
 
         <h2 css={sectionTitle}>보내는 사람</h2>
         <div css={inputContainer}>
@@ -305,11 +318,11 @@ const OrderPage = () => {
             css={inputField} 
             type="text" 
             placeholder="이름을 입력하세요." 
-            value={senderName}
-            onChange={(e) => setSenderName(e.target.value)}
+            value={formState.senderName}
+            onChange={(e) => updateFormState('senderName', e.target.value)}
           />
         </div>
-        {senderNameError && <div css={errorMessage}>{senderNameError}</div>}
+        {errorState.senderNameError && <div css={errorMessage}>{errorState.senderNameError}</div>}
         
         {/* 받는 사람 정보 */}
         <h2 css={sectionTitle}>받는 사람</h2>
@@ -319,33 +332,33 @@ const OrderPage = () => {
             css={inputField} 
             type="text" 
             placeholder="이름을 입력하세요." 
-            value={receiverName}
-            onChange={(e) => setReceiverName(e.target.value)}
+            value={formState.receiverName}
+            onChange={(e) => updateFormState('receiverName', e.target.value)}
           />
         </div>
-        {receiverNameError && <div css={errorMessage}>{receiverNameError}</div>}
+        {errorState.receiverNameError && <div css={errorMessage}>{errorState.receiverNameError}</div>}
         <div css={inputContainer}>
           <div css={inputLabel}>전화번호</div>
           <input 
             css={inputField} 
             type="text" 
             placeholder="전화번호를 입력하세요." 
-            value={phoneNumber}
-            onChange={(e) => setPhoneNumber(e.target.value)}
+            value={formState.phoneNumber}
+            onChange={(e) => updateFormState('phoneNumber', e.target.value)}
           />
         </div>
-        {phoneNumberError && <div css={errorMessage}>{phoneNumberError}</div>}
+        {errorState.phoneNumberError && <div css={errorMessage}>{errorState.phoneNumberError}</div>}
         <div css={inputContainer}>
           <div css={inputLabel}>수량</div>
           <input 
             css={quantityInput} 
             type="number" 
             min="1" 
-            value={quantity}
-            onChange={(e) => setQuantity(Number(e.target.value))}
+            value={formState.quantity}
+            onChange={(e) => updateFormState('quantity', Number(e.target.value))}
           />
         </div>
-        {quantityError && <div css={errorMessage}>{quantityError}</div>}
+        {errorState.quantityError && <div css={errorMessage}>{errorState.quantityError}</div>}
         
         {/* 상품 정보 */}
         <h2 css={sectionTitle}>상품 정보</h2>
