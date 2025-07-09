78a21e0bf9785ed420136523cdcd32ef24f47435
whyyhyh <judynam03@gmail.com>
2025-07-09 22:33:13 +0900

    feat: 로그인 기능 수정사항

--- src/pages/Login/LoginPage.tsx
+++ src/pages/Login/LoginPage.tsx
@@ -1,16 +1,19 @@
-// src/pages/LoginPage.tsx
-import styled from '@emotion/styled';
-import logo from '@/resources/images/kakao_logo.svg';
-// 경로를 '@/components/LoginFormSection'로 고정
-import { LoginForm } from "./components/LoginFormSection";
-const LoginPage = () => (
-  <Wrapper>
-    <Logo src={logo} alt="카카오 공식 로고" />
-    <LoginForm />
-  </Wrapper>
-);
+// src/pages/Login/LoginPage.tsx
+import React from 'react'
+import styled from '@emotion/styled'
+import logo from '@/resources/images/kakao_logo.svg'
+import { LoginForm } from '@/pages/Login/components/LoginFormSection'  // ← 절대 경로로 변경
 
-export default LoginPage;
+const LoginPage: React.FC = () => {
+  return (
+    <Wrapper>
+      <Logo src={logo} alt="카카오 공식 로고" />
+      <LoginForm />
+    </Wrapper>
+  )
+}
+
+export default LoginPage
 
 const Wrapper = styled.main`
   width: 100%;
@@ -19,9 +22,9 @@ const Wrapper = styled.main`
   flex-direction: column;
   align-items: center;
   justify-content: center;
-`;
+`
 
 const Logo = styled.img(({ theme }) => ({
   width: '5.5rem',
-  color: theme.colors.scale.gray900, // 여기도 scale 사용
-}));
+  // img 태그엔 color가 적용되지 않으니, 필요하다면 filter나 background-color 등을 사용하세요.
+}))
--- src/pages/Login/components/LoginFormSection/LoginForm.tsx
+++ src/pages/Login/components/LoginFormSection/LoginForm.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { useNavigate, useSearchParams } from 'react-router';
+import { useNavigate, useSearchParams } from 'react-router-dom';
 import { ROUTE_PATH } from '@/pages/Routes';
 import { LoginFormSection } from './index';
 
