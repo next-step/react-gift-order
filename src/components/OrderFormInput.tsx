da14c8f55d0d8ae447be619092fdd1f277458d17
changhee-cho <changhee@kangwon.ac.kr>
2025-07-09 14:12:31 +0900

    fix: 누락된 OrderFromInput.tsx 복원

---
+++ src/components/OrderFormInput.tsx
@@ -0,0 +1,49 @@
+import { css } from '@emotion/react';
+import theme from '@src/styles/tokens/index';
+
+interface Props extends React.InputHTMLAttributes<HTMLInputElement> {
+  error?: string;
+}
+
+const inputStyle = css`
+  width: 100%;
+  box-sizing: border-box;
+  color: ${theme.colors.textDefault};
+  transition: border-color 200ms;
+  border-style: solid;
+  min-height: 2.75rem;
+  font-size: ${theme.typography.body1Regular.fontSize};
+  font-weight: ${theme.typography.body1Regular.fontWeight};
+  line-height: ${theme.typography.body1Regular.lineHeight};
+  padding: 8px 12px;
+  border-width: 1px;
+  border-radius: 8px;
+  border-color: ${theme.colors.borderDefault};
+`;
+
+const inputErrorStyle = css`
+  border-color: red;
+`;
+
+const errorText = css`
+  font-weight: ${theme.typography.label2Regular.fontWeight};
+  font-size: ${theme.typography.label2Regular.fontSize};
+  color: red;
+  margin-top: 4px;
+  margin-bottom: 0;
+  text-align: left;
+`;
+
+const OrderFormInput = ({ error, ...inputProps }: Props) => {
+  return (
+    <div>
+      <input
+        css={[inputStyle, error && inputErrorStyle]}
+        {...inputProps}
+      />
+      {error && <p css={errorText}>{error}</p>}
+    </div>
+  );
+};
+
+export default OrderFormInput;
