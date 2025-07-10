55829170abda95d0ce53d2291f1baa38185df5fb
Injun Jeong <jijun897@naver.com>
2025-07-11 03:02:38 +0900

    fix: validation 함수에 undefined를 허용하면서 발생한 타입 불일치 수정

--- src/hooks/useFormInput.ts
+++ src/hooks/useFormInput.ts
@@ -1,16 +1,16 @@
 import { useState } from "react";
 
-type ValidatorFunction = (value: string) => string;
+type ValidatorFunction = (value: string) => string | undefined;
 
 const useFormInput = (validator: ValidatorFunction) => {
   const [value, setValue] = useState("");
-  const [error, setError] = useState("");
+  const [error, setError] = useState<string | undefined>(undefined);
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const newValue = e.target.value;
     setValue(newValue);
     if (!validator(newValue)) {
-      setError("");
+      setError(undefined);
     }
   };
 
