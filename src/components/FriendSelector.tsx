ec8759087a94ca36479619242073a84afc57043f
anseonghyeon <anseonghyeon@gmail.com>
2025-07-10 13:37:55 +0900

    refactor(friend-selector): FriendSelector 컴포넌트의 디자인 토큰값 변경과 네이밍을 개선

--- src/components/FriendSelector.tsx
+++ src/components/FriendSelector.tsx
@@ -4,17 +4,16 @@ import { useUser } from '@/hooks/useUser';
 
 const FriendSelectorWrapper = styled.div`
   width: auto;
-  height: 65px;
-  padding: 14px;
+  padding: ${({ theme }) => theme.spacing.spacing4} ${({ theme }) => theme.spacing.spacing3};
   background-color: ${({ theme }) => theme.colors.gray.gray200};
 `;
 
 const FriendSelectorBox = styled.div`
   width: auto;
-  height: auto;
-  padding: 13px;
+  height: ${({ theme }) => theme.spacing.spacing12};
+  padding: ${({ theme }) => theme.spacing.spacing3} ${({ theme }) => theme.spacing.spacing4};
   background-color: ${({ theme }) => theme.colors.gray.gray00};
-  border-radius: 15px;
+  border-radius: 16px;
   cursor: pointer;
 
   display: flex;
@@ -23,41 +22,38 @@ const FriendSelectorBox = styled.div`
 `;
 
 const FriendSelectorBoxBtn = styled.div`
-  width: 40px;
-  height: 40px;
+  width: ${({ theme }) => theme.spacing.spacing10};
+  height: ${({ theme }) => theme.spacing.spacing10};
   background-color: ${({ theme }) => theme.colors.brand.kakaoYellow};
-  border-radius: 15px;
+  border-radius: 16px;
 
   display: flex;
   justify-content: center;
-
-  &::before {
-    content: '+';
-    font-size: 34px;
-    font-weight: 100;
-    color: ${({ theme }) => theme.colors.gray.gray1000};
-  }
+  align-items: center;
 `;
 
-const FriendSelectorBoxText = styled.p`
-  font-size: ${({ theme }) => theme.typography.subtitle.subtitle1Bold.fontSize};
-  font-weight: ${({ theme }) =>
-    theme.typography.subtitle.subtitle1Bold.fontWeight};
-  line-height: ${({ theme }) =>
-    theme.typography.subtitle.subtitle1Bold.lineHeight};
-  margin-left: 12px;
+const FriendSelectorBoxTxt = styled.p`
+  font-size: ${({ theme }) => theme.typography.body.body1Bold.fontSize};
+  font-weight: ${({ theme }) => theme.typography.body.body1Bold.fontWeight};
+  line-height: ${({ theme }) => theme.typography.body.body1Bold.lineHeight};
+  margin-left: ${({ theme }) => theme.spacing.spacing3};
 `;
 
 function FriendSelector() {
-  const userId = useUser();
+  const userId = useUser(); // session storage에 있는 로그인 정보
 
   return (
     <FriendSelectorWrapper>
       <FriendSelectorBox>
-        <FriendSelectorBoxBtn></FriendSelectorBoxBtn>
-        <FriendSelectorBoxText>
+        <FriendSelectorBoxBtn>
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#2a3038" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-plus" aria-hidden="true">
+            <path d="M5 12h14"></path>
+            <path d="M12 5v14"></path>
+          </svg>
+        </FriendSelectorBoxBtn>
+        <FriendSelectorBoxTxt>
           {userId && userId + '님! '}선물할 친구를 선택해 주세요.
-        </FriendSelectorBoxText>
+        </FriendSelectorBoxTxt>
       </FriendSelectorBox>
     </FriendSelectorWrapper>
   );
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -57,7 +57,7 @@ const NavBarLoginBtn = styled.button`
 
 function NavBar() {
   const navigate = useNavigate();
-  const userId = useUser();
+  const userId = useUser(); // session storage에 있는 로그인 정보
 
   function handleBackNavigation() {
     if(window.history.length > 1) {
@@ -67,6 +67,8 @@ function NavBar() {
     }
   }
 
+  // TODO: 좀 손 봐야할듯
+  // session storage에 있는 로그인 정보로 로그인이 되어있으면 my page로 아니면 login페이지로 유도
   function handleLoginClick() {
     if (userId !== '') {
       navigate('/my');
