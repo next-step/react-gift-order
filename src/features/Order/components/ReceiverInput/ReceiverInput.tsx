6e41a7692f552c3605f1c5f4092a4fc3954d306e
daaoooy <y020d226y@daum.net>
2025-07-11 17:42:09 +0900

    feat(receiver) : 받는 사람 기능 고도화(단체 주문 기능)
    - 받는 사람을 여러 명 (최대 10명) 등록할 수 있는 기능을 추가함
    - 10명의 정보가 정확하게 입력되지 않으면 정보 반영 되지 않는 기능 추가함
    - 받는 사람들끼리는 전화번호가 중복될 수 없는 기능 추가함
    - 그 외 조건은 이전과 동일함
    
    - ReceiverInput -> 받는 사람 입력 폼
    - ReceiverInputList -> 받는 사람들 쭉 보여주는 리스트
    - ReceiverModal -> 받는 사람을 추가하는 모달창
    - ReceiverSection -> 최종 반영하여 주문하기 폼에 보여주는 받는 사람 부분

--- src/features/Order/components/ReceiverInput/ReceiverInput.tsx
+++ src/features/Order/components/ReceiverInput/ReceiverInput.tsx
@@ -1,64 +1,95 @@
 import * as S from './ReceiverInput.styles'
+import type {
+  FieldError,
+  UseFormRegister,
+  UseFormGetValues,
+} from 'react-hook-form'
+import type { Order } from '@/features/Order/hooks/useOrderForm'
+import MyButton from '@/component/Button/Button'
 
 interface ReceiverInputProps {
-  receiver: string
-  setReceiver: (name: string) => void
-  phoneNumber: string
-  setPhoneNumber: (phone: string) => void
-  quantity: number
-  setQuantity: (quantity: number) => void
-  errors: {
-    receiver: string
-    phoneNumber: string
-    quantity: string
+  index: number
+  register: UseFormRegister<Order>
+  getValues: UseFormGetValues<Order>
+  error?: {
+    receiver?: FieldError
+    phone?: FieldError
+    quantity?: FieldError
   }
+  onRemove: () => void
 }
 
 const ReceiverInput = ({
-  receiver,
-  setReceiver,
-  phoneNumber,
-  setPhoneNumber,
-  quantity,
-  setQuantity,
-  errors,
+  index,
+  register,
+  error,
+  onRemove,
+  getValues,
 }: ReceiverInputProps) => {
   return (
     <S.Container>
-      <S.Title>받는 사람</S.Title>
+      <S.TitleContainer>
+        <S.Title>받는 사람 {index + 1}</S.Title>
+        <MyButton onClick={onRemove} size="verySmall" variant="outlined">
+          삭제
+        </MyButton>
+      </S.TitleContainer>
+
       <S.InputContainer>
         <S.InputLabel>이름</S.InputLabel>
         <S.InputText
-          value={receiver}
-          onChange={(e) => setReceiver(e.target.value)}
-          placeholder="이름을 입력하세요."
-          isError={!!errors.receiver}
+          placeholder="이름"
+          {...register(`receivers.${index}.receiver`, {
+            required: '이름을 입력해주세요',
+          })}
+          isError={!!error?.receiver}
         />
-        {errors.receiver && <S.ErrorText>{errors.receiver}</S.ErrorText>}
+        {error?.receiver && <S.ErrorText>{error.receiver.message}</S.ErrorText>}
       </S.InputContainer>
-
       <S.InputContainer>
         <S.InputLabel>전화번호</S.InputLabel>
         <S.InputText
-          value={phoneNumber}
-          onChange={(e) => setPhoneNumber(e.target.value)}
-          placeholder="전화번호를 입력하세요."
-          isError={!!errors.phoneNumber}
+          placeholder="전화번호"
+          {...register(`receivers.${index}.phone`, {
+            required: '전화번호를 입력해주세요',
+            pattern: {
+              value: /^010\d{8}$/,
+              message: '전화번호 형식이 올바르지 않습니다',
+            },
+            validate: (currentValue) => {
+              const receivers = getValues('receivers')
+              const phoneNumbers = receivers.map((receiver) => receiver.phone)
+              const sameNumberCount = phoneNumbers.filter(
+                (phone) => phone === currentValue
+              ).length
+              if (sameNumberCount > 1) {
+                return '중복된 전화번호가 있습니다.'
+              }
+              return true
+            },
+          })}
+          isError={!!error?.phone}
         />
-        {errors.phoneNumber && <S.ErrorText>{errors.phoneNumber}</S.ErrorText>}
+        {error?.phone && <S.ErrorText>{error.phone.message}</S.ErrorText>}
       </S.InputContainer>
+
       <S.InputContainer>
         <S.InputLabel>수량</S.InputLabel>
         <S.InputText
           type="number"
-          value={quantity}
-          min={1}
-          onChange={(e) => setQuantity(Number(e.target.value))}
-          placeholder="구매 수량"
-          isError={!!errors.quantity}
+          placeholder="수량"
+          {...register(`receivers.${index}.quantity`, {
+            required: '수량을 입력해주세요',
+            min: {
+              value: 1,
+              message: '수량은 최소 1개 이상이어야 합니다',
+            },
+          })}
+          isError={!!error?.quantity}
         />
-        {errors.quantity && <S.ErrorText>{errors.quantity}</S.ErrorText>}
+        {error?.quantity && <S.ErrorText>{error.quantity.message}</S.ErrorText>}
       </S.InputContainer>
+      <S.Divider />
     </S.Container>
   )
 }
