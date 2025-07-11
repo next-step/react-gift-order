53168edfb590a90abd8ac71a2bbf31efec81ab28
김성진 <gimseongjin@gimseongjin-ui-noteubug.local>
2025-07-12 01:11:56 +0900

    fix: validator.ts에 z.string().trim() 로직 추가

--- src/utils/validator.ts
+++ src/utils/validator.ts
@@ -3,10 +3,12 @@ import { z } from "zod";
 export const loginSchema = z.object({
   email: z
     .string()
+    .trim()
     .min(1, "이메일을 입력해주세요.")
     .email("이메일은 이메일 형식으로 입력해주세요."),
   password: z
     .string()
+    .trim()
     .min(1, "비밀번호를 입력해주세요.")
     .min(8, "비밀번호는 최소 8자 이상이어야 합니다."),
 });
@@ -14,14 +16,15 @@ export const loginSchema = z.object({
 export type LoginFormValues = z.infer<typeof loginSchema>;
 
 export const orderSchema = z.object({
-  message: z.string().min(1, "메시지를 입력해주세요."),
-  sender: z.string().min(1, "보내는 사람 이름이 반드시 필요해요."),
+  message: z.string().trim().min(1, "메시지를 입력해주세요."),
+  sender: z.string().trim().min(1, "보내는 사람 이름이 반드시 필요해요."),
 });
 
 export const receiverSchema = z.object({
-  name: z.string().min(1, "받는 사람 이름이 반드시 필요해요."),
+  name: z.string().trim().min(1, "받는 사람 이름이 반드시 필요해요."),
   phone: z
     .string()
+    .trim()
     .regex(/^010\d{8}$/, "전화번호는 01012341234 형식으로 입력하세요."),
   quantity: z.coerce.number().min(1, "수량은 1개 이상이어야 해요."),
 });
