a0f92da8348ad81e38d6ff70b026d9867ffe1b5d
kimgho <kgh67811@knu.ac.kr>
2025-07-11 16:49:32 +0900

    feat(constant): 중복된 전화번호 에러 메시지 추가

--- src/constants/order-error-messages.ts
+++ src/constants/order-error-messages.ts
@@ -15,6 +15,7 @@ export const ORDER_ERROR_MESSAGE = {
     REQUIRED: "이름을 입력해주세요.",
     REQUIRED_TEL: "전화번호를 입력해주세요.",
     INVALID_TEL_FORMAT: "올바른 전화번호 형식이 아닙니다.",
+    DUPLICATE_TEL_NUMBER: "중복된 전화번호가 있습니다.",
   },
   QUANTITY: {
     INVALID_QUANTITY: "구매 수량은 1개 이상이어야 합니다.",
