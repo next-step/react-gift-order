8c12d38348624354f3e760e0899bd22806b5f3eb
dib3474 <dib3474@gmail.com>
2025-07-11 17:51:25 +0900

    refactor: 불필요한 코드 및 함수 삭제

--- src/pages/Login/LoginPage.tsx
+++ src/pages/Login/LoginPage.tsx
@@ -8,7 +8,6 @@ import { useAuth } from "@/contexts/authContext";
 
 const LoginPage = () => {
   const { user, onChange, onBlur, errorMsg } = useLoginInput();
-  // useForm();
   const { login } = useAuth();
 
   const handleLoginSubmit = (event: React.FormEvent<HTMLFormElement>) => {
