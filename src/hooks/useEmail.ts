75c54f085d36a73b5a7eb7291a61478181303c85
thgus0 <sohyun10521052@gmail.com>
2025-07-11 16:42:18 +0900

    refactor: useInput 훅 추가 및 useEmail과 usePassword 로직 수정

--- src/hooks/useEmail.ts
+++ src/hooks/useEmail.ts
@@ -1,21 +1,12 @@
-import { useMemo, useState } from 'react'
+import { useInput } from './useInput'
+
+function validateEmail(value: string) {
+  if (!value) return 'ID를 입력해주세요.'
+  const isEmailValid = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(value)
+  if (!isEmailValid) return 'ID는 이메일 형식으로 입력해주세요.'
+  return ''
+}
 
 export function useEmail() {
-  const [value, setValue] = useState('')
-  const [touched, setTouched] = useState(false)
-
-  const error = useMemo(() => {
-    if (!value) return 'ID를 입력해주세요.'
-    const isEmailValid = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(value)
-    if (!isEmailValid) return 'ID는 이메일 형식으로 입력해주세요.'
-    return ''
-  }, [value])
-
-  return {
-    value,
-    change: setValue,
-    touched,
-    error,
-    onBlur: () => setTouched(true),
-  }
+  return useInput(validateEmail)
 }
--- src/hooks/usePassword.ts
+++ src/hooks/usePassword.ts
@@ -1,20 +1,11 @@
-import { useState, useMemo } from 'react'
+import { useInput } from './useInput'
+
+function validatePassword(value: string) {
+  if (!value) return 'PW를 입력해주세요.'
+  if (value.length < 8) return 'PW는 최소 8글자 이상이어야 합니다.'
+  return ''
+}
 
 export function usePassword() {
-  const [value, setValue] = useState('')
-  const [touched, setTouched] = useState(false)
-
-  const error = useMemo(() => {
-    if (!value) return 'PW를 입력해주세요.'
-    if (value.length < 8) return 'PW는 최소 8글자 이상이어야 합니다.'
-    return ''
-  }, [value])
-
-  return {
-    value,
-    change: setValue,
-    touched,
-    error,
-    onBlur: () => setTouched(true),
-  }
+  return useInput(validatePassword)
 }
