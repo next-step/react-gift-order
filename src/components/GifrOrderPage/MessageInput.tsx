75637b00457627eb1a10ab1f60a48c69939ee15b
jinhyun <jin71744@gmail.com>
2025-07-12 00:55:04 +0900

    style: MessageInput css 수정

--- src/components/GifrOrderPage/MessageInput.tsx
+++ src/components/GifrOrderPage/MessageInput.tsx
@@ -2,8 +2,9 @@ import styled from '@emotion/styled';
 
 const TextArea = styled.textarea(({ theme }) => ({
   width: '100%',
+  maxWidth: '660px',
+  margin: '0 auto',
   height: theme.spacing.spacing10,
-  margin: theme.spacing.spacing3,
   padding: theme.spacing.spacing3,
   borderRadius: theme.spacing.spacing2,
   border: `1px solid ${theme.colors.semantic.borderDefault}`,
