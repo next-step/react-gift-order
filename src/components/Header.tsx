e0634ddd6b278640497fe4ec03df9f72746c1d6d
cktjgus <cktjgus04@naver.com>
2025-07-12 14:35:29 +0900

    3단계 피드백 : path 상수화 적용

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,6 +3,7 @@ import styled from "@emotion/styled";
 import { useNavigate } from "react-router-dom";
 import IconButton from "./IconButton";
 import { STORAGE_KEY } from "@/constants/storage";
+import { PATH } from "@/paths";
 
 export default function Header() {
   const navigate = useNavigate();
@@ -13,13 +14,13 @@ export default function Header() {
         <IconButton aria-label="뒤로가기" onClick={() => navigate(-1)}>
           <FiChevronLeft size={24} />
         </IconButton>
-        <Title onClick={() => navigate("/")}>선물하기</Title>
+        <Title onClick={() => navigate(PATH.HOME)}>선물하기</Title>
         <IconButton
           aria-label="마이페이지"
           onClick={() => {
             const userInfo = sessionStorage.getItem(STORAGE_KEY.USER_INFO);
-            if (userInfo) navigate("/my");
-            else navigate("/login");
+            if (userInfo) navigate(PATH.MY);
+            else navigate(PATH.LOGIN);
           }}
         >
           <FiUser size={24} />
