ffa3dc47302db48228c2805b29cdb6f71d8e607b
Jiwon Jeong <81281798+jjw5655@users.noreply.github.com>
2025-07-14 14:50:11 +0900

    chore: 사용되지 않는 커스텀 훅 제거
    - useRecipientModal, useOrderForm 훅 파일 삭제
    - 두 훅 모두 어떤 컴포넌트에서도 사용되지 않는 데드 코드였음
    - hooks 배럴파일(index.ts)에서 해당 export 제거

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,8 +1,6 @@
 export { useLoginForm } from './useLoginForm';
 export { useAuth } from '@/contexts';
-export { useOrderForm } from './useOrderForm';
 export { useMultipleRecipientsForm } from './useMultipleRecipientsForm';
-export { useRecipientModal } from './useRecipientModal';
 export { useForm } from './useForm';
 export { useLocalStorageState } from './useLocalStorageState';
 export { useBodyScrollLock } from './useBodyScrollLock';
