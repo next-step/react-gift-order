1d98f9464f864e605a4b7cc3e0f4bd1cc25974cb
whyyhyh <judynam03@gmail.com>
2025-07-15 22:31:58 +0900

    fix: import 부분 들여쓰기 재정렬

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,11 @@ import { theme } from './styles/theme';
 import { BaseLayout } from './components/Layout/BaseLayout';
 import { Navigation } from './components/Layout/Navigation';
 import { Routes, Route } from 'react-router-dom';
-import HomePage   from '@/pages/Home/Page';
-import LoginPage  from '@/pages/Login/LoginPage';
-import OrderPage  from '@/pages/Home/OrderPage';
-import MyPage     from '@/pages/MyPage/MyPage';
-import NotFound   from '@/pages/NotFound/Page';
+import HomePage from '@/pages/Home/Page';
+import LoginPage from '@/pages/Login/LoginPage';
+import OrderPage from '@/pages/Home/OrderPage';
+import MyPage from '@/pages/MyPage/MyPage';
+import NotFound from '@/pages/NotFound/Page';
 import { RequireAuth } from '@/components/RequireAuth';
 
 const App = () => {
